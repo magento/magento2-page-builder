10adc16d962592d23d0b3af624a528b519fb85ba
Hob Adams <hob@gene.co.uk>
2016-04-29 17:16:42 +0100

    further work on the page builder blocks

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@ var config = {
         "*": {
             'slick': 'Gene_BlueFoot/js/resource/jquery/slick/slick',
             'fancybox': 'Gene_BlueFoot/js/resource/jquery/fancybox/jquery.fancybox.pack',
+            'highlight': 'Gene_BlueFoot/js/resource/highlight/highlight.pack',
             'bluefoot/tabs': 'Gene_BlueFoot/js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
             'bluefoot/accordion': 'Gene_BlueFoot/js/resource/jquery/bluefoot-accordion/jquery.bluefoot.accordion.min'
         }
@@ -10,13 +11,13 @@ var config = {
     paths: {
         'slick': 'js/resource/jquery/slick/slick',
         'fancybox': 'js/resource/jquery/fancybox/jquery.fancybox.pack',
+        'highlight': 'js/resource/highlight/highlight.pack',
         'bluefoot/tabs': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
         'bluefoot/accordion': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.accordion.min'
-
-
     },
     'shim': {
         'slick' : ['jquery'],
-        'fancybox' : ['jquery']
+        'fancybox' : ['jquery'],
+        'highlight' : []
     }
 };
