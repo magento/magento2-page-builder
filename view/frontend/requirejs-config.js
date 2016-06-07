43a050ec076468ebb94ab7286fba29e8a818e429
Hob Adams <hob@gene.co.uk>
2016-06-07 15:25:45 +0100

    fix normalise heights js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,7 +5,8 @@ var config = {
             'fancybox': 'Gene_BlueFoot/js/resource/jquery/fancybox/jquery.fancybox.pack',
             'highlight': 'Gene_BlueFoot/js/resource/highlight/highlight.pack',
             'bluefoot/tabs': 'Gene_BlueFoot/js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
-            'bluefoot/accordion': 'Gene_BlueFoot/js/resource/jquery/bluefoot-accordion/jquery.bluefoot.accordion.min'
+            'bluefoot/accordion': 'Gene_BlueFoot/js/resource/jquery/bluefoot-accordion/jquery.bluefoot.accordion.min',
+            'bluefoot/normalise-heights': 'Gene_BlueFoot/js/resource/jquery/bluefoot-normalise-heights/jquery.bluefoot.normaliseHeights.min'
         }
     },
     paths: {
@@ -13,7 +14,8 @@ var config = {
         'fancybox': 'js/resource/jquery/fancybox/jquery.fancybox.pack',
         'highlight': 'js/resource/highlight/highlight.pack',
         'bluefoot/tabs': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
-        'bluefoot/accordion': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.accordion.min'
+        'bluefoot/accordion': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.accordion.min',
+        'bluefoot/normalise-heights': 'js/resource/jquery/bluefoot-normalise-heights/jquery.bluefoot.normaliseHeights.min'
     },
     'shim': {
         'slick' : ['jquery'],
