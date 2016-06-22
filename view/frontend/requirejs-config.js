d4b7f2e538182bd281d10f1af6fe3bf06278c568
Hob Adams <hob@gene.co.uk>
2016-06-22 10:34:36 +0100

    update require js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,21 +1,11 @@
 var config = {
-    map: {
-        "*": {
-            'slick': 'Gene_BlueFoot/js/resource/jquery/slick/slick',
-            'fancybox': 'Gene_BlueFoot/js/resource/jquery/fancybox/jquery.fancybox.pack',
-            'highlight': 'Gene_BlueFoot/js/resource/highlight/highlight.pack',
-            'bluefoot/tabs': 'Gene_BlueFoot/js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
-            'bluefoot/accordion': 'Gene_BlueFoot/js/resource/jquery/bluefoot-accordion/jquery.bluefoot.accordion.min',
-            'bluefoot/normalise-heights': 'Gene_BlueFoot/js/resource/jquery/bluefoot-normalise-heights/jquery.bluefoot.normaliseHeights.min'
-        }
-    },
     paths: {
-        'slick': 'js/resource/jquery/slick/slick',
-        'fancybox': 'js/resource/jquery/fancybox/jquery.fancybox.pack',
-        'highlight': 'js/resource/highlight/highlight.pack',
-        'bluefoot/tabs': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
-        'bluefoot/accordion': 'js/resource/jquery/bluefoot-tabs/jquery.bluefoot.accordion.min',
-        'bluefoot/normalise-heights': 'js/resource/jquery/bluefoot-normalise-heights/jquery.bluefoot.normaliseHeights.min'
+        'slick': 'Gene_BlueFoot/js/resource/jquery/slick/slick',
+        'fancybox': 'Gene_BlueFoot/js/resource/jquery/fancybox/jquery.fancybox.pack',
+        'highlight': 'Gene_BlueFoot/js/resource/highlight/highlight.pack',
+        'bluefoot/tabs': 'Gene_BlueFoot/js/resource/jquery/bluefoot-tabs/jquery.bluefoot.tabs.min',
+        'bluefoot/accordion': 'Gene_BlueFoot/js/resource/jquery/bluefoot-tabs/jquery.bluefoot.accordion.min',
+        'bluefoot/normalise-heights': 'Gene_BlueFoot/js/resource/jquery/bluefoot-normalise-heights/jquery.bluefoot.normaliseHeights.min'
     },
     shim: {
         'slick': {
