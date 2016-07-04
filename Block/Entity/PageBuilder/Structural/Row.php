b8021db64118597c4d79bbd724858cbb69b83f70
Dave Macaulay <dave@gene.co.uk>
2016-07-04 09:46:45 +0100

    Fix row templates front-end rendering

--- Block/Entity/PageBuilder/Structural/Row.php
+++ Block/Entity/PageBuilder/Structural/Row.php
@@ -11,5 +11,23 @@ namespace Gene\BlueFoot\Block\Entity\PageBuilder\Structural;
  */
 class Row extends AbstractStructural
 {
+    /**
+     * Base path of templates
+     * @var string
+     */
+    protected $_path = 'Gene_BlueFoot::pagebuilder/structural/core/row/';
 
+    /**
+     * Render block HTML
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (($template = $this->getFormData('template')) && $template != 'default.phtml') {
+            $this->setTemplate($this->_path . $template);
+        }
+
+        return parent::_toHtml();
+    }
 }
