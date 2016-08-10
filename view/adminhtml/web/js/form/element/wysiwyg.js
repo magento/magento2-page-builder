c608dbdd01943e8fa6f66ba864e686a010163d5c
Dave Macaulay <dave@gene.co.uk>
2016-08-10 11:18:59 +0100

    Get KO working with individual instances

--- view/adminhtml/web/js/form/element/wysiwyg.js
+++ view/adminhtml/web/js/form/element/wysiwyg.js
@@ -8,55 +8,33 @@ define([
     'Magento_Ui/js/lib/view/utils/async',
     'ko',
     'uiRegistry',
-    'Magento_Variable/variables'
-], function (Wysiwyg, $, ko, registry) {
+    'bluefoot/stage',
+    'Magento_Variable/variables',
+], function (Wysiwyg, $, ko, registry, Stage) {
     'use strict';
 
     /**
      * Extend the original WYSIWYG with added BlueFoot functionality
      */
     return Wysiwyg.extend({
-        bluefoot: {
-            stageTemplate: 'Gene_BlueFoot/component/core/stage.html',
+        defaults: {
             stage: {},
-            content: ko.observableArray([]),
-
-            /**
-             * Retrieve the panel from the registry
-             *
-             * @returns {*}
-             */
-            getBlueFootPanel: function () {
-                return registry.get('bluefoot-panel');
-            },
-
-            /**
-             * Bind a click event to the BlueFoot init button
-             *
-             * @param node
-             */
-            bindBlueFootButton: function (node) {
-                $(node).prevAll('.buttons-set').find('.init-gene-bluefoot').on('click', this.buildBlueFoot.bind(this));
-            },
-
-            /**
-             * Handle a click event requesting that we build BlueFoot
-             *
-             * @param event
-             */
-            buildBlueFoot: function (event) {
-                event.stopPropagation();
-                var panel;
-                if (panel = this.getBlueFootPanel()) {
-                    panel.buildPanel();
-                } else {
-                    console.warn('Unable to locate the BlueFoot panel for initialization.');
-                }
-            },
-
-            addRow: function () {
-                console.log('addRow');
+            stageContent: [],
+            links: {
+                stage: {},
+                stageContent: []
             }
+        },
+
+        /**
+         *
+         * @returns {exports}
+         */
+        initObservable: function () {
+            this._super()
+                .observe('value stageContent');
+
+            return this;
         },
 
         /**
@@ -65,12 +43,56 @@ define([
          */
         setElementNode: function (node) {
             $(node).bindings({
-                value: this.value
+                value: this.value,
+                stageContent: this.stageContent
             });
 
-            // Bind our event to the WYSIWYG editor
-            this.bluefoot.bindBlueFootButton(node);
-        }
+            this.bindBlueFootButton(node);
+        },
+
+        /**
+         * Retrieve the panel from the registry
+         *
+         * @returns {*}
+         */
+        getBlueFootPanel: function () {
+            return registry.get('bluefoot-panel');
+        },
+
+        /**
+         * Bind a click event to the BlueFoot init button
+         *
+         * @param node
+         */
+        bindBlueFootButton: function (node) {
+            $(node).prevAll('.buttons-set').find('.init-gene-bluefoot').on('click', this.buildBlueFoot.bind(this));
+        },
+
+        /**
+         * Handle a click event requesting that we build BlueFoot
+         *
+         * @param event
+         */
+        buildBlueFoot: function (event) {
+            event.stopPropagation();
+            var panel;
+            if (panel = this.getBlueFootPanel()) {
+                panel.buildPanel();
+
+                this.stage = new Stage(this, this.stageContent);
+            } else {
+                console.warn('Unable to locate the BlueFoot panel for initialization.');
+            }
+        },
+
+        /**
+         * Return the BlueFoot stage templage
+         *
+         * @returns {string}
+         */
+        getStageTemplate: function () {
+            return 'Gene_BlueFoot/component/core/stage.html';
+        },
 
     });
 });
