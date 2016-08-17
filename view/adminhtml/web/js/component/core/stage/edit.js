7e6e5d3acedd3aea12f4d337347cfcd1ad604206
Dave Macaulay <dave@gene.co.uk>
2016-08-17 11:36:29 +0100

    Basic edit panel opening with no content

---
+++ view/adminhtml/web/js/component/core/stage/edit.js
@@ -0,0 +1,29 @@
+/**
+ * - Edit.js
+ * Allows users to edit elements within the stage
+ *
+ * @author Dave Macaulay <dave@gene.co.uk>
+ */
+define([
+    'uiComponent',
+    'ko',
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function(Component, ko, jQuery, Modal) {
+
+    /**
+     * Extend the component for BlueFoot panel specific functionality
+     */
+    return Component.extend({
+
+        /**
+         * Initialize the panel component
+         *
+         * @param config
+         */
+        initialize: function (config) {
+            this._super();
+        },
+
+    });
+});
