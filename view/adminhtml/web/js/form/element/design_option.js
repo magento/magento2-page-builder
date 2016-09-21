6dc7aa9027294361bd3a2ae94bd532ac8bff25c1
Dave Macaulay <dave@gene.co.uk>
2016-09-21 16:32:38 +0100

    Build design options UI component

--- view/adminhtml/web/js/form/element/design_option.js
+++ view/adminhtml/web/js/form/element/design_option.js
@@ -2,6 +2,7 @@ define([
     'Magento_Ui/js/form/element/abstract',
     'ko',
     'jquery',
+    'jquery/ui',
     'Gene_BlueFoot/js/resource/jquery/colorpicker/js/colorpicker'
 ], function (AbstractField, ko, $) {
     'use strict';
@@ -19,11 +20,17 @@ define([
             borderLeft: '-',
             borderColor: '#000000',
             borderTextColor: '#FFFFFF',
+            borderStyle: 'inherit',
+            borderRadius: '0',
+            borderSlider: false,
 
             paddingTop: '-',
             paddingRight: '-',
             paddingBottom: '-',
             paddingLeft: '-',
+
+            backgroundColor: '',
+            backgroundTextColor: '#000000',
 
             listens: {
                 marginTop: 'updateValue',
@@ -35,12 +42,16 @@ define([
                 borderRight: 'updateValue',
                 borderBottom: 'updateValue',
                 borderLeft: 'updateValue',
-                borderColor: 'updateValue',
+                borderColor: 'updateValue updateBorderTextColor',
+                borderStyle: 'updateValue',
+                borderRadius: 'updateValue updateSlider',
 
                 paddingTop: 'updateValue',
                 paddingRight: 'updateValue',
                 paddingBottom: 'updateValue',
-                paddingLeft: 'updateValue'
+                paddingLeft: 'updateValue',
+
+                backgroundColor: 'updateValue updateBackgroundTextColor'
             }
         },
 
@@ -64,12 +75,15 @@ define([
                 this.borderBottom(obj.border[2]);
                 this.borderLeft(obj.border[3]);
                 this.borderColor(obj.borderColor);
-                this.updateBorderTextColor();
+                this.borderStyle(obj.borderStyle);
+                this.borderRadius(obj.borderRadius);
 
                 this.paddingTop(obj.padding[0]);
                 this.paddingRight(obj.padding[1]);
                 this.paddingBottom(obj.padding[2]);
                 this.paddingLeft(obj.padding[3]);
+
+                this.backgroundColor(obj.backgroundColor);
             }
 
             return this;
@@ -80,8 +94,9 @@ define([
          */
         initObservable: function () {
             return this._super().observe('marginTop marginRight marginBottom marginLeft ' +
-                'borderTop borderRight borderBottom borderLeft borderColor borderTextColor ' +
-                'paddingTop paddingRight paddingBottom paddingLeft');
+                'borderTop borderRight borderBottom borderLeft borderColor borderTextColor borderStyle borderRadius ' +
+                'paddingTop paddingRight paddingBottom paddingLeft ' +
+                'backgroundColor backgroundTextColor');
         },
 
         /**
@@ -92,25 +107,27 @@ define([
                 margin: [this.marginTop(), this.marginRight(), this.marginBottom(), this.marginLeft()],
                 border: [this.borderTop(), this.borderRight(), this.borderBottom(), this.borderLeft()],
                 borderColor: this.borderColor(),
-                padding: [this.paddingTop(), this.paddingRight(), this.paddingBottom(), this.paddingLeft()]
+                borderStyle: this.borderStyle(),
+                borderRadius: this.borderRadius(),
+                padding: [this.paddingTop(), this.paddingRight(), this.paddingBottom(), this.paddingLeft()],
+                backgroundColor: this.backgroundColor()
             };
 
             this.value(JSON.stringify(obj));
-
-            this.updateBorderTextColor();
         },
 
         /**
          * Render the colour picker, fired from afterRender on element
          *
          * @param element
+         * @param field
          */
-        renderColorPicker: function (element) {
+        renderColorPicker: function (element, field) {
             // Attach the color picker
             $(element).ColorPicker({
-                color: this.borderColor(),
+                color: this[field](),
                 onChange: function (hsb, hex, rgb) {
-                    this.borderColor(hex);
+                    this[field](hex);
                 }.bind(this)
             });
         },
@@ -122,6 +139,42 @@ define([
             if (this.borderColor()) {
                 this.borderColor(this.borderColor().replace('#', ''));
                 this.borderTextColor((parseInt(this.borderColor(), 16) > 0xffffff / 1.75) ? '#000000' : '#FFFFFF');
+            }
+        },
+
+        /**
+         * Update the background text colour based on the selection
+         */
+        updateBackgroundTextColor: function () {
+            if (this.backgroundColor()) {
+                this.backgroundColor(this.backgroundColor().replace('#', ''));
+                this.backgroundTextColor((parseInt(this.backgroundColor(), 16) > 0xffffff / 1.75) ? '#000000' : '#FFFFFF');
+            }
+        },
+
+        /**
+         * Render the slider element
+         *
+         * @param element
+         */
+        renderSlider: function (element) {
+            $(element).slider({
+                value: this.borderRadius(),
+                min: 0,
+                max: 50,
+                slide: function( event, ui ) {
+                    this.borderRadius(ui.value);
+                }.bind(this)
+            });
+            this.borderSlider = $(element);
+        },
+
+        /**
+         * Update the slider
+         */
+        updateSlider: function () {
+            if (this.borderSlider) {
+                this.borderSlider.slider("option", "value", this.borderRadius());
             }
         }
 
