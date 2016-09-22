99eb6290bccc8cce865313c9183de46a1c9ff638
Dave Macaulay <dave@gene.co.uk>
2016-09-22 10:23:57 +0100

    Remove console logs, oops!

--- view/adminhtml/web/js/form/element/video.js
+++ view/adminhtml/web/js/form/element/video.js
@@ -35,7 +35,6 @@ define([
             if (vimeo.test(this.value())){
                 embedUrl = '//player.vimeo.com/video/$1';
                 iframeUrl = this.value().replace(vimeo, embedUrl);
-                console.log(iframeUrl);
                 this.type('vimeo');
                 this.iframeSrc(iframeUrl);
             }
@@ -44,7 +43,6 @@ define([
             if (youtube.test(this.value())){
                 embedUrl = 'http://www.youtube.com/embed/$2';
                 iframeUrl = this.value().replace(youtube, embedUrl);
-                console.log(iframeUrl);
                 this.type('youtube');
                 this.iframeSrc(iframeUrl);
             }
