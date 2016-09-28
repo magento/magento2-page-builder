61d3fe6672f950cd4492165b6415f2e07d99fdd4
Aidan Threadgold <aidan@gene.co.uk>
2016-09-28 13:28:30 +0100

    Template Saving

--- Controller/Adminhtml/Stage/Template/Index.php
+++ Controller/Adminhtml/Stage/Template/Index.php
@@ -17,17 +17,31 @@ class Index extends \Magento\Backend\App\Action
     protected $resultJsonFactory;
 
     /**
+     * @var \Gene\BlueFoot\Model\ResourceModel\Stage\Template\CollectionFactory
+     */
+    protected $templateFactory;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
      * Index constructor.
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Gene\BlueFoot\Model\ResourceModel\Stage\Template\CollectionFactory $templateFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         parent::__construct($context);
 
         $this->resultJsonFactory = $resultJsonFactory;
+        $this->templateFactory = $templateFactory;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -37,12 +51,41 @@ class Index extends \Magento\Backend\App\Action
      */
     public function execute()
     {
-        return $this->resultJsonFactory->create()->setData([
-            'templates' => [
-                ['id' => '99', 'label' => 'From ajax'],
-                ['id' => '11', 'label' => 'From ajax also'],
-                ['id' => '121', 'label' => 'From ajax also']
-            ]
+        $collection = $this->templateFactory->create();
+        $collection->getSelect()->reset('columns');
+        $collection->getSelect()->columns([
+            'main_table.template_id',
+            'main_table.name',
+            'main_table.structure',
+            'main_table.preview',
+            'main_table.pinned'
         ]);
+        $results = $collection->load();
+
+        $json = [];
+        foreach ($results as $item) {
+            $json[] = [
+                'id' => $item->getId(),
+                'label' => $item->getName(),
+                'preview' => $this->getPreviewUrl($item->getPreview()),
+                'structure' => $item->getStructure()
+            ];
+        }
+
+        return $this->resultJsonFactory->create()->setData([
+            'templates' => $json
+        ]);
+    }
+
+    /**
+     * URL to the media path
+     * @param string $path
+     * @return string
+     */
+    public function getPreviewUrl($path)
+    {
+        return
+            $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) .
+            'bluefoot' . DIRECTORY_SEPARATOR . 'previews' . DIRECTORY_SEPARATOR . $path;
     }
 }
