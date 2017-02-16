0213fb6eebf48b06a5a844d42c9c879832a99f01
Dave Macaulay <dave@gene.co.uk>
2017-02-16 09:55:37 -0600

    Removal of legacy plugin system, re-organise requirejs includes in main.js

--- view/adminhtml/web/js/main.js
+++ view/adminhtml/web/js/main.js
@@ -1,58 +1,47 @@
 define([
     'uiComponent',
-    'bluefoot/stage/build'
-], function (Component, StageBuild) {
-
+    'bluefoot/stage/build',
+    'bluefoot/hook',
+    'bluefoot/stage',
+    'bluefoot/jquery',
+    'bluefoot/cms-config',
+    'bluefoot/modal'
+], function (
+    Component,
+    StageBuild,
+    Hook,
+    StageClass,
+    jQuery,
+    InitConfig
+) {
+    /**
+     * Create a new componet for BlueFoot to intercept
+     */
     return Component.extend({
         initialize: function () {
             this._super();
             this._initialized = false;
         },
         init: function () {
-            var self = this;
-            // The app requires the core hook system to be running very early on
-            require(['bluefoot/hook'], function (Hook) {
+            this._initialized = true;
 
-                // Declare our plugins system
-                require(['bluefoot/plugins'], function (Plugins) {
+            // Detect and load any saved page builder data
+            StageBuild.init();
 
-                    // Prepare the plugin aspect of the system
-                    Plugins.prepare(function () {
+            // Remove any other click events
+            jQuery(document).off('click', InitConfig.init_button_class);
+            jQuery(document).on('click', InitConfig.init_button_class, function (event) {
 
-                        // Initialize the basic config to load in plugins
-                        require(['bluefoot/stage', 'bluefoot/jquery', 'bluefoot/cms-config', 'bluefoot/modal'], function (StageClass, jQuery, InitConfig) {
+                /**
+                 * Create a new instance of the stage
+                 *
+                 * Each Gene CMS instance is ran by a stage, this handles all operations of the "page builder" which
+                 * is refereed to in code as the stage
+                 */
+                var Stage = new StageClass();
+                Stage.init(jQuery(event.currentTarget));
 
-                            Plugins.load('onPageLoad', function () {
-
-                                self._initialized = true;
-
-                                // Detect and load any saved page builder data
-                                StageBuild.init();
-
-                                // Remove any other click events
-                                jQuery(document).off('click', InitConfig.init_button_class);
-                                jQuery(document).on('click', InitConfig.init_button_class, function (event) {
-
-                                    /**
-                                     * Create a new instance of the stage
-                                     *
-                                     * Each Gene CMS instance is ran by a stage, this handles all operations of the "page builder" which
-                                     * is refereed to in code as the stage
-                                     */
-                                    var Stage = new StageClass();
-                                    Stage.init(jQuery(event.currentTarget));
-
-                                }.bind(this));
-
-                            });
-
-                        });
-
-                    });
-
-                });
-
-            });
+            }.bind(this));
         }
     });
 });
