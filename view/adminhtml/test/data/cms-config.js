032a86341d4d16c7bcced7bbedcc31bd5649ec64
Dave Macaulay <dave@gene.co.uk>
2017-03-16 15:05:01 +0000

    Include a column definition in cms-config.js for use in config & column tests

--- view/adminhtml/test/data/cms-config.js
+++ view/adminhtml/test/data/cms-config.js
@@ -2,6 +2,12 @@ define([], function () {
     return {
         config_url: '/testConfig',
         form_key: 'test-form-key',
-        get_value_test: true
+        get_value_test: true,
+        column_definitions: [
+            {
+                className: 'test',
+                breakpoint: '0.5'
+            }
+        ]
     };
 });
