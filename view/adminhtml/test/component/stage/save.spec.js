6e91da7d9b4ae593ea2ea3d1ab469f01790f5b33
Dave Macaulay <dave@gene.co.uk>
2017-03-16 12:25:12 +0000

    Tests for stage/save.js

---
+++ view/adminhtml/test/component/stage/save.spec.js
@@ -0,0 +1,60 @@
+/**
+ * JS Unit Test for stage.js
+ *
+ * @author Dave Macaulay <dave@gene.co.uk>
+ */
+define([
+    'ko',
+    'bluefoot/stage',
+    'bluefoot/stage/save'
+], function (ko, Stage, Save) {
+    describe("Gene_BlueFoot/js/component/stage/save", function () {
+        var stage, save;
+        beforeEach(function () {
+            stage = {
+                stageContent: ko.observableArray([]),
+                parent: {
+                    value: function () {}
+                },
+                toJSON: function () {
+                    return {'test': 1}
+                }
+            };
+            save = new Save(stage);
+            save.input = {
+                val: function () {
+                    return '{"test":1}';
+                }
+            };
+        });
+
+        it("will call update on stageContent change", function (done) {
+            var originalUpdate = save.update;
+            save.update = function () {
+                done();
+            };
+            stage.stageContent.push(1);
+            save.update = originalUpdate;
+        });
+
+        it("update will update stage parent value", function (done) {
+            stage.parent.value = function () {
+                done();
+            }
+            save.update();
+        });
+
+        it("will return json string when converting the stage", function () {
+            expect(save.stageToJSON()).toEqual('{"test":1}');
+        });
+
+        it("deleted items are recorded in save.deleted", function () {
+            save.delete('testDelete');
+            expect(save.deleted[0]).toEqual('testDelete')
+        });
+
+        it("will return object when getting data from input", function () {
+            expect(save.getData()).toEqual({'test': 1});
+        });
+    });
+});
