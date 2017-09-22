a1ab7679609a534f1d65ff8a51dd396bb5369153
Dave Macaulay <dmacaulay@magento.com>
2017-09-22 13:54:25 +0200

    Start building image preview view model

--- view/adminhtml/web/ts/component/block/preview/image.ts
+++ view/adminhtml/web/ts/component/block/preview/image.ts
@@ -1,5 +1,45 @@
 import PreviewBlock from "./block";
+import ko from "knockout";
+import Config from "../../config";
+
 
 export default class PreviewImageBlock extends PreviewBlock {
+    image: KnockoutObservable<string>;
+    loading: KnockoutObservable<boolean> = ko.observable(false);
+    imageUrl: KnockoutComputed<string> = ko.computed(() => {
+        if (this.image()) {
+            return Config.getInitConfig('media_url') + this.image().replace('/media/', '');;
+        }
+        return '';
+    });
 
+    uploadUrl() {
+        return Config.getPluginConfig('gene_widget_upload', 'upload_url');
+    }
+
+    attachmentSuccess() {
+        return (file, response, bindKey) => {
+            if (response.file) {
+                this.parent.data()[bindKey] = response.file;
+                this.parent.data.valueHasMutated();
+                setTimeout(function () {
+                    this.loading(false);
+                }.bind(this), 50);
+            } else {
+                alert($t("Your image could not be uploaded"));
+            }
+        };
+    }
+
+    attachmentDrop() {
+        return (event) => {
+            jQuery(event.target).parents('.dz-drag-hover').removeClass('dz-drag-hover');
+            this.loading(true);
+        };
+    }
+
+    attachmentError() {
+        this.loading(false);
+        alert($t("Your image could not be uploaded"));
+    }
 }
