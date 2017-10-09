46ccda6e201f2dae2cc66f0fe2539f989b26b24b
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2017-10-09 16:57:13 +0200

    MAGETWO-66176 Bluefoot Column Configuration
      - Single repository for functional testing and JavaScript unit testing
      - Add type-script and babel transformations
      - Documentation for new repository structure

--- dev/tests/js/README.md
+++
@@ -1,5 +0,0 @@
-# Example RequireJS Jest Adapter for Magento
-
-1. Run `yarn install` in this directory
-4. `npm test`
-
--- dev/tests/js/unit/utils/array.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const arrayUtils = require('::Gene_BlueFoot/js/utils/array');
-
-// 1.
-
-test('it works', function () {
-
-});
