fcae196b7c66cb209968ed467e6d654f18a6472e
Dave Macaulay <dmacaulay@magento.com>
2017-10-20 13:08:47 +0200

    Resovle symlink module imports
    This is resolving a core issue where modules that are symlink’d need to return the real relative path not the emulated one

--- app/code/Gene/BlueFoot/registration.php
+++ app/code/Gene/BlueFoot/registration.php
@@ -1,6 +1,15 @@
 <?php
+/**
+ * Correctly resolve the module to the document root of the Magento installation.
+ * This is required due to the way template validation behaves with symlink'd
+ * modules. All templates have to be within the base path of the installation
+ * to pass validation, symlink'd files will retain their original path thus
+ * failing.
+ */
+$dir = defined('BP') && strpos(__DIR__, BP) !== 0 ? BP . substr(__DIR__, strlen(dirname(__DIR__, 4))) : __DIR__;
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Gene_BlueFoot',
-    __DIR__
+    $dir
 );
