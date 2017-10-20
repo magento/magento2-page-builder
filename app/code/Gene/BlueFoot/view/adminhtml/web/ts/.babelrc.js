a293218bc58ac8e762f8075a05764224d7a49c41
Dave Macaulay <dmacaulay@magento.com>
2017-10-20 15:11:01 +0200

    MAGETWO-66176: Bluefoot Column Configuration
     - Include transform-class-properties plugin in babel

--- app/code/Gene/BlueFoot/view/adminhtml/web/ts/.babelrc.js
+++ app/code/Gene/BlueFoot/view/adminhtml/web/ts/.babelrc.js
@@ -2,6 +2,9 @@ const magentoClasses = require('./magentoClasses');
 
 module.exports = {
     presets: [
+        {
+            plugins: ['transform-class-properties']
+        },
         ['es6-to-magento-amd', {magentoClasses: magentoClasses}]
     ],
     plugins: ['transform-typescript']
