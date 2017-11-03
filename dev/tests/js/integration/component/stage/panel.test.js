ab86d0aef3e0b49d2a259b3377b164b60c710c82
Anthoula Wojczak <awojczak@magento.com>
2017-11-03 11:28:29 -0500

    MAGETWO-71566: Filtering Bluefoot content elements on the Panel
    - optimize jest tests
    - add additional test cases

--- dev/tests/js/integration/component/stage/panel.test.js
+++ dev/tests/js/integration/component/stage/panel.test.js
@@ -1,57 +1,79 @@
+function searchInPanel(keyword) {
+    (require('setup-jquery'))(jest);
+    (require('setup-loader'))(jest);
+    initConfiguration();
+    panel = new (require('js/component/stage/panel'))();
+    panel.search(panel, {currentTarget: {value: keyword}});
+
+    return panel.searchResults().map((value) => value.label());
+}
+
+function initConfiguration() {
+    jest.mock(
+        '::advanced-cms-init-config',
+        () => {
+            const fullConfiguration = Object.assign({}, require('init-config'));
+            const contentType = (label, key, visible) => {
+                return {
+                    "name": key,
+                    "label": label,
+                    "icon": "icon-bluefoot-text",
+                    "form": "text_form",
+                    "contentType": "",
+                    "group": "general",
+                    "fields": {"textarea": {"default": "Type your text here..."}},
+                    "visible": visible || visible === undefined,
+                    "preview_template": "Gene_BlueFoot/component/block/preview/text.html",
+                    "preview_component": "Gene_BlueFoot/js/component/block/preview/block",
+                    "component": "Gene_BlueFoot/js/component/block/block"
+                };
+            };
+
+            fullConfiguration.contentTypes = {
+                block: contentType('Block', 'block'),
+                column: contentType('Column', 'column'),
+                code: contentType('Code', 'code'),
+                invisible_block: contentType('Invisible Block', 'invisible_block', false),
+                oblong: contentType('Oblong', 'oblong'),
+                segmented_block: contentType('Segmented Block', 'segmented_block')
+            };
+
+            return fullConfiguration;
+        },
+        {virtual: true}
+    );
+}
+
 describe('searching in panel', () => {
-    (require('../../../wrapper/setup-jquery'))(jest);
+    it('returns no results if no matches found', () => {
+        expect(searchInPanel('not matching')).toEqual([]);
+    });
 
-    jest.mock('::advanced-cms-init-config', () => require('init-config'), {virtual: true});
-    jest.mock('::Gene_BlueFoot/js/component/loader', () => require('jest-magento2/fake-require'));
+    it('returns only visible blocks', () => {
+        expect(searchInPanel('Invisible Block')).toEqual([]);
+    });
 
-    const fakeRequire = require('jest-magento2/fake-require');
-    fakeRequire.set('Gene_BlueFoot/js/component/block/preview/block', require('js/component/block/preview/block'));
-    fakeRequire.set('Gene_BlueFoot/js/component/block/preview/image', require('js/component/block/preview/image'));
+    it('returns results matching full phrase', () => {
+        expect(searchInPanel('Segmented Block')).toEqual(
+            ['Segmented Block']
+        );
+    });
 
-    let panel;
+    it('returns matches for one character', () => {
+        expect(searchInPanel('b')).toEqual(
+            ['Block', 'Segmented Block']
+        );
+    });
 
-    beforeEach(() => {
-        (require('setup-stage'))(jest);
-        panel = new (require('::js/component/stage/panel'))();
+    it('returns matches for two characters', () => {
+        expect(searchInPanel('co')).toEqual(
+            ['Column', 'Code']
+        );
     });
 
     it('returns matches for three characters', () => {
-        const element = jQuery('<input>');
-        element.bind('keyup', function(event) {
-            this.value = 'blo';
-            panel.search(panel, event);
-            const searchResults = panel.searchResults();
-            for (let i = 0; i < searchResults.length; i++) {
-                const label = searchResults[i].config.label;
-                expect(label).toEqual(expect.stringMatching(/Blo/));
-            }
-        });
-        var e = jQuery.Event('keyup');
-        jQuery(element).trigger(e);
-    });
-    it('returns matches for one character', () => {
-        const element = jQuery('<input>');
-        element.bind('keyup', function(event) {
-            this.value = 'b';
-            panel.search(panel, event);
-            const searchResults = panel.searchResults();
-            for (let i = 0; i < searchResults.length; i++) {
-                const label = searchResults[i].config.label;
-                expect(label).toEqual(expect.stringMatching(/B/));
-            }
-        });
-        var e = jQuery.Event('keyup');
-        jQuery(element).trigger(e);
-    });
-    it('returns no matches', () => {
-        const element = jQuery('<input>');
-        element.bind('keyup', function(event) {
-            this.value = 'abcdef';
-            panel.search(panel, event);
-            const searchResultsLength = panel.searchResults().length;
-            expect(searchResultsLength).toEqual(0);
-        });
-        var e = jQuery.Event('keyup');
-        jQuery(element).trigger(e);
+        expect(searchInPanel('blo')).toEqual(
+            ['Block', 'Segmented Block']
+        );
     });
 });
