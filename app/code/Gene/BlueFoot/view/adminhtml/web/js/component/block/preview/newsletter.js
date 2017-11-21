1053c4e9e0052888aadefd20673993db9c5af620
Igor Melnikov <imelnikov@magento.com>
2017-11-21 16:10:10 -0600

    MAGETWO-83937: Newsletter
    - newsletter content type

---
+++ app/code/Gene/BlueFoot/view/adminhtml/web/js/component/block/preview/newsletter.js
@@ -0,0 +1,49 @@
+define(["knockout", "./block", "../../config"], function (_knockout, _block, _config) {
+  function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
+
+  var ContentBlock =
+  /*#__PURE__*/
+  function (_PreviewBlock) {
+    _inheritsLoose(ContentBlock, _PreviewBlock);
+
+    /**
+     * PreviewBlock constructor
+     *
+     * @param {Block} parent
+     * @param {Object} config
+     */
+    function ContentBlock(parent, config) {
+      var _this;
+
+      _this = _PreviewBlock.call(this, parent, config) || this;
+
+      _this.updateDataValue('html', _knockout.observable(''));
+
+      _this.parent.stage.store.subscribe(function (data) {
+        if (_this.data.title() === '') {
+          return;
+        }
+
+        var url = _config.getInitConfig('preview_url'),
+            requestData = {
+          role: _this.config.name,
+          'button_text': _this.data.button_text,
+          'button_label': _this.data.button_label,
+          'placeholder': _this.data.placeholder,
+          'title': _this.data.title
+        };
+
+        jQuery.post(url, requestData, function (response) {
+          _this.updateDataValue('html', response.content.trim());
+        });
+      }, _this.parent.id);
+
+      return _this;
+    }
+
+    return ContentBlock;
+  }(_block);
+
+  return ContentBlock;
+});
+//# sourceMappingURL=newsletter.js.map
