a5dbe9f678f67f6ce0132f2367203cffc6b1d545
Dave Macaulay <dmacaulay@magento.com>
2017-11-24 12:37:46 +0100

    MAGETWO-83922: Buttons
    - Add button content type

---
+++ app/code/Gene/BlueFoot/view/adminhtml/ts/js/component/format/read/buttons.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import ReadInterface from "../read-interface";
+import Config from "../../../component/config";
+import _ from 'underscore';
+import {DataObject} from "../../data-store";
+
+export default class Buttons implements ReadInterface {
+
+    /**
+     * Read heading type and title from the element
+     *
+     * @param element HTMLElement
+     * @returns {Promise<any>}
+     */
+    public read(element: HTMLElement): Promise<any> {
+        let response: DataObject = {
+            'buttons': []
+        };
+
+        // Iterate through the tabs and retrieve their content
+        _.forEach(element.querySelectorAll('a'), (node, index) => {
+            response.buttons[index] = {
+                text: node.firstChild.innerText,
+                link: node.getAttribute('href'),
+            }
+        });
+
+        return Promise.resolve(response);
+    }
+}
