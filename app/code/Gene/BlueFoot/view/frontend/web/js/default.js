6d3313f737177e40c5c284c95da254e0c9713990
Dave Macaulay <dmacaulay@magento.com>
2017-12-01 11:29:39 +0100

    MAGETWO-84779: make-slider-functional-on-the-frontend
    - Resolve styling issues
    - Ensure back-end options are used on front-end

--- app/code/Gene/BlueFoot/view/frontend/web/js/default.js
+++ app/code/Gene/BlueFoot/view/frontend/web/js/default.js
@@ -13,9 +13,15 @@ requirejs(['jquery', 'fancybox', 'highlight', 'slick'], function ($, fancybox, h
             );
         });
 
-        $("div[data-role='advanced-slider']").slick();
-
-
-
+        $("div[data-role='advanced-slider']").each(function (index, element) {
+            $(element).slick({
+                autoplay: $(element).data('autoplay') === 1,
+                autoplaySpeed: $(element).data('autoplay-speed') || 0,
+                fade: $(element).data('fade') === 1,
+                infinite: $(element).data('is-infinite') === 1,
+                arrows: $(element).data('show-arrows') === 1,
+                dots: $(element).data('show-dots') === 1
+            });
+        });
     });
 });
