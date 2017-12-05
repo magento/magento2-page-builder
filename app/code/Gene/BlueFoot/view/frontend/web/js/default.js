8404058ff917baae1a65830a382309a48955fb77
Michael Yu <hyu@magento.com>
2017-12-05 14:24:23 -0600

    MAGETWO-84934: fix-bluefoot-advance-slider-alignment - Remove debounce and delay from frontend slickjs.

--- app/code/Gene/BlueFoot/view/frontend/web/js/default.js
+++ app/code/Gene/BlueFoot/view/frontend/web/js/default.js
@@ -3,40 +3,34 @@
  * See COPYING.txt for license details.
  */
 
-requirejs(['jquery', 'fancybox', 'highlight', 'underscore', 'slick'], function ($, fancybox, hljs, _) {
+requirejs(['jquery', 'fancybox', 'highlight', 'slick'], function ($, fancybox, hljs) {
     $(document).ready(function() {
         $('.bluefoot-lightbox').fancybox();
 
-        $('pre code:not(.hljs)').each(function(i, block) {
+        $('pre code:not(.hljs)').each(function (i, block) {
             $(block).html(
                 hljs.highlight('html', $(block).html()).value
             );
         });
 
-        $("div[data-role='advanced-slider']").each(function (index, element) {
+        $('div[data-role="advanced-slider"]').each(function (index, element) {
+            if ($(element) && $(element).length > 0) {
+                /**
+                 * Prevent each slick slider from being initialized more than once which could throw an error.
+                 */
+                if($(element).hasClass('slick-initialized')) {
+                    $(element).slick('unslick');
+                }
 
-            /**
-             * Assign a debounce and delay to the init of slick to ensure the DOM has updated
-             *
-             */
-            _.debounce(function(){
-                _.delay(function(){
-                    if ($(element) && $(element).length > 0) {
-                        if($(element).hasClass('slick-initialized')){
-                            $(element).slick('unslick');
-                        }
-
-                        $(element).slick({
-                            autoplay: $(element).data('autoplay') === 1,
-                            autoplaySpeed: $(element).data('autoplay-speed') || 0,
-                            fade: $(element).data('fade') === 1,
-                            infinite: $(element).data('is-infinite') === 1,
-                            arrows: $(element).data('show-arrows') === 1,
-                            dots: $(element).data('show-dots') === 1
-                        });
-                    }
-                }, 100);
-            }, 20)();
+                $(element).slick({
+                    autoplay: $(element).data('autoplay') === 1,
+                    autoplaySpeed: $(element).data('autoplay-speed') || 0,
+                    fade: $(element).data('fade') === 1,
+                    infinite: $(element).data('is-infinite') === 1,
+                    arrows: $(element).data('show-arrows') === 1,
+                    dots: $(element).data('show-dots') === 1
+                });
+            }
         });
     });
 });
