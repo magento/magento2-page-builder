3b82f36ccb4c8aeb97b98516035418b211a7044f
Anthoula Wojczak <awojczak@magento.com>
2017-12-14 10:17:39 -0600

    MAGETWO-84660: Add parallax background tab and check on frontend
    - refactor css binding for parallax class

--- app/code/Gene/BlueFoot/view/adminhtml/web/ts/js/component/block/row.ts
+++ app/code/Gene/BlueFoot/view/adminhtml/web/ts/js/component/block/row.ts
@@ -31,4 +31,15 @@ export default class Row extends Block {
         }
         return _.extend(super.getStyle(), styleAttributes);
     }
+
+    /**
+     * Is parallax enabled?
+     *
+     * @returns {boolean}
+     */
+
+    isParallaxEnabled() {
+        let data = this.getData();
+        return !(data.enable_parallax === "" || data.enable_parallax === 'undefined' || data.enable_parallax === "0");
+    }
 }
