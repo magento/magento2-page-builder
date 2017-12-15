9f1e6b4ccf492fdf8fd850ed6c67d1f5fbb2a2f8
Dave Macaulay <dmacaulay@magento.com>
2017-12-15 14:04:59 -0600

    MAGETWO-85331: Content type anchor interface and renderer for anchor
    - Add anchor renderer

---
+++ app/code/Gene/BlueFoot/Setup/DataConverter/Renderer/Anchor.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Gene\BlueFoot\Setup\DataConverter\Renderer;
+
+use Gene\BlueFoot\Setup\DataConverter\RendererInterface;
+use Gene\BlueFoot\Setup\DataConverter\EntityHydratorInterface;
+use Gene\BlueFoot\Setup\DataConverter\StyleExtractorInterface;
+
+class Anchor implements RendererInterface
+{
+    /**
+     * @var StyleExtractorInterface
+     */
+    private $styleExtractor;
+
+    /**
+     * @var EntityHydratorInterface
+     */
+    private $entityHydrator;
+
+    public function __construct(
+        StyleExtractorInterface $styleExtractor,
+        EntityHydratorInterface $entityHydrator
+    ) {
+        $this->styleExtractor = $styleExtractor;
+        $this->entityHydrator = $entityHydrator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(array $itemData, array $additionalData = [])
+    {
+        $eavData = $this->entityHydrator->hydrate($itemData);
+
+        $rootElementAttributes = [
+            'data-role' => 'anchor',
+            'class' => $eavData['css_classes'] ?? '',
+            'id' => $eavData['anchor_id']
+        ];
+
+        $style = $this->styleExtractor->extractStyle($itemData['formData']);
+        if ($style) {
+            $rootElementAttributes['style'] = $style;
+        }
+
+        $rootElementHtml = '<span';
+        foreach ($rootElementAttributes as $attributeName => $attributeValue) {
+            $rootElementHtml .= $attributeValue ? " $attributeName=\"$attributeValue\"" : '';
+        }
+        $rootElementHtml .= '></span>';
+
+        return $rootElementHtml;
+    }
+}
