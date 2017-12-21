0b9e53427439af859213d8a10dd837b1e82c4c60
Igor Melnikov <imelnikov@magento.com>
2017-12-20 20:36:56 -0600

    MAGETWO-85352: Renderer for video
    - refactor

--- app/code/Gene/BlueFoot/Setup/DataConverter/Renderer/Video.php
+++ app/code/Gene/BlueFoot/Setup/DataConverter/Renderer/Video.php
@@ -59,7 +59,7 @@ class Video implements RendererInterface
             $rootElementAttributes['style'] = $style;
         }
 
-        $rootElementHtml = '<iframe frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""';
+        $rootElementHtml = '<iframe frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen';
         foreach ($rootElementAttributes as $attributeName => $attributeValue) {
             $rootElementHtml .= $attributeValue ? " $attributeName=\"$attributeValue\"" : '';
         }
