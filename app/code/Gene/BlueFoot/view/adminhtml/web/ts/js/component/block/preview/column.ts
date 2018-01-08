94ac4a1efeefc6db0de911650573625b264fafb3
Anthoula Wojczak <awojczak@magento.com>
2018-01-07 21:16:35 -0600

    MAGETWO-85509: [Spike] Flex box in admin preview
    - Add preview template for column
    - Remove column width field from form as it's determined by flexbox
    - Add display:flex wrapper to columns elements

---
+++ app/code/Gene/BlueFoot/view/adminhtml/web/ts/js/component/block/preview/column.ts
@@ -0,0 +1,68 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import ko from "knockout";
+import _ from "underscore";
+import PreviewBlock from "./block";
+import Block from "../block"
+import StyleAttributeMapper from "../../format/style-attribute-mapper";
+import StyleAttributeFilter from "../../format/style-attribute-filter";
+
+export default class Row extends PreviewBlock {
+    columnStyles: KnockoutComputed<{}>;
+    // afterColumnRenders: KnockoutComputed<{}>;
+
+    /**
+     * @param {Block} parent
+     * @param {Object} config
+     */
+    constructor(parent: Block, config: object) {
+        super(parent, config);
+        const styleAttributeMapper = new StyleAttributeMapper();
+        const styleAttributeFilter = new StyleAttributeFilter();
+
+        this.columnStyles = ko.computed(() => {
+            // Extract data values our of observable functions
+            let styles = styleAttributeMapper.toDom(
+                styleAttributeFilter.filter(
+                    _.mapObject(this.data, (value) => {
+                        if (ko.isObservable(value)) {
+                            return value();
+                        }
+                        return value;
+                    })
+                )
+            );
+
+            // The style attribute mapper converts images to directives, override it to include the correct URL
+            if (this.data.background_image && typeof this.data.background_image()[0] === 'object') {
+                styles['backgroundImage'] = 'url(' + this.data.background_image()[0].url + ')';
+            }
+
+            styles['flex-grow'] = 1;
+
+            return styles;
+        });
+
+        // Force the columnStyles to update on changes to stored style attribute data
+        Object.keys(styleAttributeFilter.allowedAttributes).forEach((key) => {
+            if (ko.isObservable(this.data[key])) {
+                this.data[key].subscribe(() => {
+                    this.columnStyles.notifySubscribers();
+                });
+            }
+        });
+
+        this.afterColumnRenders = () => {
+            // Not wrapping correctly here, but does if ran once after all elements render
+            jQuery('.row-container').find('.bluefoot-column').parent('.bluefoot-structure-wrapper').addClass('column-flex');
+            jQuery('.row-container').each(function(index) {
+                if ( jQuery(this).find('.column-flex').parent('.column-flex-wrapper').length === 0 ) {
+                    jQuery(this).find('.column-flex').wrapAll('<div class="column-flex-wrapper" style="display: flex">');
+                }
+            });
+        };
+    }
+}
