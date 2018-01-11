946f8196dba9e517597e3bab9fabbc90095cbbc7
Robert He <rohe@magento.com>
2018-01-10 18:06:50 -0600

    MAGETWO-86192: Removing unnecessary code excluding Template Manager (PHP)
     - removed JS files related to Jest

--- dev/tests/js/wrapper/setup-loader.js
+++
@@ -1,7 +0,0 @@
-module.exports = (jest) => {
-    jest.mock(
-        '::Magento_PageBuilder/js/component/loader',
-        () => require('jest-magento2/fake-require'),
-        {virtual: true}
-    );
-};
