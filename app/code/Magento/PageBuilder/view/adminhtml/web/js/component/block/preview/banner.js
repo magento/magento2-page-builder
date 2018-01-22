1857c6feba934a30c2d2ba9d918ed9ed6a2efce4
Anthoula Wojczak <awojczak@magento.com>
2018-01-22 14:59:13 -0600

    MAGETWO-86836: Banner displayed with Poster Appearance
    - add margins and padding defaults to Edit Banner form
    - adjust css for banner overlay and button
    - add link_url and content attr ko bindings to template
    - calculate HEX to RGBA
    - calculate percent to decimal for alpha
    - add margins and padding style bindings
    - add translations

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/banner.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/banner.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-define(["./block"], function (_block) {
+define(["mage/translate", "./block"], function (_translate, _block) {
   function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
   var Banner =
@@ -39,10 +39,40 @@ define(["./block"], function (_block) {
 
 
     _proto.getPreviewOverlayAttributes = function getPreviewOverlayAttributes() {
-      var backgroundColor = this.data.show_overlay() === "never_show" ? "transparent" : "rgba(0,0,0,0.3)";
+      var backgroundColor = this.data.show_overlay() === "never_show" ? "transparent" : this.getRgba();
       return {
         style: "min-height: 250px; background-color: " + backgroundColor + ";"
       };
+    };
+    /**
+     * Convert HEX to RGBA for transparent overlay for the preview
+     *
+     * @returns {string}
+     */
+
+
+    _proto.getRgba = function getRgba() {
+      if (this.data.overlay_color() !== "" && this.data.overlay_color() !== undefined) {
+        var colors = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(this.data.overlay_color()),
+            red = parseInt(colors[1], 16),
+            green = parseInt(colors[2], 16),
+            blue = parseInt(colors[3], 16),
+            alpha = this.convertPercentToDecimal(this.data.overlay_transparency());
+        return "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
+      } else {
+        return "transparent";
+      }
+    };
+    /**
+     * Convert percent to decimal for transparent overlay for the preview
+     *
+     * @param {string} value
+     * @returns {string}
+     */
+
+
+    _proto.convertPercentToDecimal = function convertPercentToDecimal(value) {
+      return (parseInt(value, 10) / 100).toString();
     };
     /**
      * Is there content in the WYSIWYG?
@@ -55,6 +85,28 @@ define(["./block"], function (_block) {
       return this.data.message() === "" || this.data.message() === undefined ? true : false;
     };
     /**
+     * Get the banner content attributes for the preview
+     *
+     * @returns {any}
+     */
+
+
+    _proto.getPreviewContentAttributes = function getPreviewContentAttributes() {
+      if (this.data.margins_and_padding() !== "" && this.data.margins_and_padding() !== undefined) {
+        var marginTop = this.data.margins_and_padding().margin.top || "0",
+            marginRight = this.data.margins_and_padding().margin.right || "0",
+            marginBottom = this.data.margins_and_padding().margin.bottom || "0",
+            marginLeft = this.data.margins_and_padding().margin.left || "0",
+            paddingTop = this.data.margins_and_padding().padding.top || "0",
+            paddingRight = this.data.margins_and_padding().padding.right || "0",
+            paddingBottom = this.data.margins_and_padding().padding.bottom || "0",
+            paddingLeft = this.data.margins_and_padding().padding.left || "0";
+        return {
+          style: "margin-top: " + marginTop + "px; " + "margin-right: " + marginRight + "px; " + "margin-bottom: " + marginBottom + "px; " + "margin-left: " + marginLeft + "px; " + "padding-top: " + paddingTop + "px; " + "padding-right: " + paddingRight + "px; " + "padding-bottom: " + paddingBottom + "px; " + "padding-left: " + paddingLeft + "px;"
+        };
+      }
+    };
+    /**
      * Get the content for the preview
      *
      * @returns {any}
@@ -63,9 +115,9 @@ define(["./block"], function (_block) {
 
     _proto.getContentHtml = function getContentHtml() {
       if (this.data.message() === "" || this.data.message() === undefined) {
-        return "Write banner text here...";
+        return (0, _translate)("Write banner text here...");
       } else {
-        return this.data.message();
+        return (0, _translate)(this.data.message());
       }
     };
     /**
@@ -77,9 +129,9 @@ define(["./block"], function (_block) {
 
     _proto.getButtonText = function getButtonText() {
       if (this.data.button_text() === "" || this.data.button_text() === undefined) {
-        return "Edit Button Text";
+        return (0, _translate)("Edit Button Text");
       } else {
-        return this.data.button_text();
+        return (0, _translate)(this.data.button_text());
       }
     };
 
--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/preview/banner.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/preview/banner.ts
@@ -3,6 +3,7 @@
  * See COPYING.txt for license details.
  */
 
+import $t from "mage/translate";
 import PreviewBlock from "./block";
 
 export default class Banner extends PreviewBlock {
@@ -13,10 +14,9 @@ export default class Banner extends PreviewBlock {
      * @returns {any}
      */
     public getPreviewBannerAttributes() {
-        let backgroundImage = "none",
-            minHeight = "250px",
-            backgroundSize = "cover";
-
+        let backgroundImage:string = "none",
+            minHeight:string = "250px",
+            backgroundSize:string = "cover";
         if (this.data.image() !== "" && this.data.image() !== undefined && this.data.image()[0] !== undefined) {
             backgroundImage = "url(" + this.data.image()[0].url + ")";
         }
@@ -29,8 +29,36 @@ export default class Banner extends PreviewBlock {
      * @returns {any}
      */
     public getPreviewOverlayAttributes() {
-        let backgroundColor = this.data.show_overlay() === "never_show" ? "transparent" : "rgba(0,0,0,0.3)";
+        let backgroundColor:string = this.data.show_overlay() === "never_show" ? "transparent" : this.getRgba();
         return {style: "min-height: 250px; background-color: " + backgroundColor + ";"};
+    }
+
+    /**
+     * Convert HEX to RGBA for transparent overlay for the preview
+     *
+     * @returns {string}
+     */
+    private getRgba() {
+        if (this.data.overlay_color() !== "" && this.data.overlay_color() !== undefined) {
+            let colors = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(this.data.overlay_color()),
+                red = parseInt(colors[1], 16),
+                green = parseInt(colors[2], 16),
+                blue = parseInt(colors[3], 16),
+                alpha = this.convertPercentToDecimal(this.data.overlay_transparency());
+            return "rgba(" + red + "," + green +"," + blue + "," + alpha + ")";
+        } else {
+            return "transparent";
+        }
+    }
+
+    /**
+     * Convert percent to decimal for transparent overlay for the preview
+     *
+     * @param {string} value
+     * @returns {string}
+     */
+    private convertPercentToDecimal(value: string) {
+        return (parseInt(value, 10) / 100).toString();
     }
 
     /**
@@ -38,8 +66,37 @@ export default class Banner extends PreviewBlock {
      *
      * @returns {boolean}
      */
-    public isBannerEmpty() {
+    public isBannerEmpty():boolean {
         return this.data.message() === "" || this.data.message() === undefined ? true : false;
+    }
+
+    /**
+     * Get the banner content attributes for the preview
+     *
+     * @returns {any}
+     */
+    public getPreviewContentAttributes() {
+        if (this.data.margins_and_padding() !== "" && this.data.margins_and_padding() !== undefined) {
+            let marginTop = this.data.margins_and_padding().margin.top || "0",
+                marginRight = this.data.margins_and_padding().margin.right || "0",
+                marginBottom = this.data.margins_and_padding().margin.bottom || "0",
+                marginLeft = this.data.margins_and_padding().margin.left || "0",
+                paddingTop = this.data.margins_and_padding().padding.top || "0",
+                paddingRight = this.data.margins_and_padding().padding.right || "0",
+                paddingBottom = this.data.margins_and_padding().padding.bottom || "0",
+                paddingLeft = this.data.margins_and_padding().padding.left || "0";
+            return {
+                style:
+                    "margin-top: " + marginTop + "px; " +
+                    "margin-right: " + marginRight + "px; " +
+                    "margin-bottom: " + marginBottom + "px; " +
+                    "margin-left: " + marginLeft + "px; " +
+                    "padding-top: " + paddingTop + "px; " +
+                    "padding-right: " + paddingRight + "px; " +
+                    "padding-bottom: " + paddingBottom + "px; " +
+                    "padding-left: " + paddingLeft + "px;"
+            };
+        }
     }
 
     /**
@@ -49,9 +106,9 @@ export default class Banner extends PreviewBlock {
      */
     public getContentHtml() {
         if (this.data.message() === "" || this.data.message() === undefined) {
-            return "Write banner text here...";
+            return $t("Write banner text here...");
         } else {
-            return this.data.message();
+            return $t(this.data.message());
         }
     }
 
@@ -62,9 +119,9 @@ export default class Banner extends PreviewBlock {
      */
     public getButtonText() {
         if (this.data.button_text() === "" || this.data.button_text() === undefined) {
-            return "Edit Button Text";
+            return $t("Edit Button Text");
         } else {
-            return this.data.button_text();
+            return $t(this.data.button_text());
         }
     }
 }
