0075589d44b4c61e63099d7745096978c215ae74
Raith Hamzah <rhamzah@magento.com>
2018-02-01 08:49:46 -0600

    MAGETWO-87267: Update Admin Preview - Created column preview class - Updated column preview template - Updated content types config to reflect changes

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/preview/column.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import ko from "knockout";
+import _ from "underscore";
+import PreviewBlock from "./block";
+import Block from "../block"
+import StyleAttributeMapper from "../../format/style-attribute-mapper";
+import StyleAttributeFilter from "../../format/style-attribute-filter";
+
+export default class Column extends PreviewBlock {
+    columnStyles: KnockoutComputed<{}>;
+    getChildren: KnockoutComputed<{}>;
+    wrapClass: KnockoutObservable<boolean> = ko.observable(false);
+
+    /**
+     * @param {Block} parent
+     * @param {Object} config
+     */
+    constructor(parent: Block, config: object) {
+        super(parent, config);
+        const styleAttributeMapper = new StyleAttributeMapper();
+        const styleAttributeFilter = new StyleAttributeFilter();
+
+        this.columnStyles = ko.computed(() => {
+            // Extract data values our of observable functions
+            let styles = styleAttributeMapper.toDom(
+                styleAttributeFilter.filter(
+                    _.mapObject(this.data, (value) => {
+                        if (ko.isObservable(value)) {
+                            return value();
+                        }
+                        return value;
+                    })
+                )
+            );
+
+            // The style attribute mapper converts images to directives, override it to include the correct URL
+            if (this.data.background_image && typeof this.data.background_image()[0] === 'object') {
+                styles['backgroundImage'] = 'url(' + this.data.background_image()[0].url + ')';
+            }
+
+            return styles;
+        });
+
+        // Force the columnStyles to update on changes to stored style attribute data
+        Object.keys(styleAttributeFilter.allowedAttributes).forEach((key) => {
+            if (ko.isObservable(this.data[key])) {
+                this.data[key].subscribe(() => {
+                    this.columnStyles.notifySubscribers();
+                });
+            }
+        });
+    }
+}
