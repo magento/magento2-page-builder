1898559ad2d4911094c9baa32e38d78a7aa66ffc
Michael Yu <hyu@magento.com>
2018-02-08 13:24:07 -0600

    MAGETWO-86971: Add new button to Buttons from stage and button configuration
    - Resolved code styling issues
    - Removed unused data-binding
    - Removed commented code

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/buttons.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/buttons.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-define(["../../block/factory", "../../config", "./block"], function (_factory, _config, _block) {
+define(["./block"], function (_block) {
   function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
   var Buttons =
@@ -7,25 +7,9 @@ define(["../../block/factory", "../../config", "./block"], function (_factory, _
   function (_PreviewBlock) {
     _inheritsLoose(Buttons, _PreviewBlock);
 
-    function Buttons(parent, config) {
-      return _PreviewBlock.call(this, parent, config) || this;
+    function Buttons() {
+      return _PreviewBlock.apply(this, arguments) || this;
     }
-    /**
-     * Add a button into the buttons container
-     */
-
-
-    var _proto = Buttons.prototype;
-
-    _proto.addButton = function addButton() {
-      var _this = this;
-
-      (0, _factory)(_config.getInitConfig("contentTypes")["button-item"], this.parent, this.stage, {}).then(function (button) {
-        _this.addChild(button);
-
-        button.edit.open();
-      });
-    };
 
     return Buttons;
   }(_block);
--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/preview/buttons.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/preview/buttons.ts
@@ -3,24 +3,7 @@
  * See COPYING.txt for license details.
  */
 
-import createBlock from "../../block/factory";
-import Config from "../../config";
-import Block from "../block";
 import PreviewBlock from "./block";
 
 export default class Buttons extends PreviewBlock {
-    constructor(parent: Block, config: object) {
-        super(parent, config);
-    }
-
-    /**
-     * Add a button into the buttons container
-     */
-    public addButton() {
-        createBlock(Config.getInitConfig("contentTypes")["button-item"],
-            this.parent, this.stage, {}).then((button) => {
-            this.addChild(button);
-            button.edit.open();
-        });
-    }
 }
