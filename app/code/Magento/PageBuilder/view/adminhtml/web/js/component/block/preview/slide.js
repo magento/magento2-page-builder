b458f2843f934d9b12f85ea6ad4e3f1cd1ffaa81
Anthoula Wojczak <awojczak@magento.com>
2018-02-13 17:27:54 -0600

    MAGETWO-87774: Rename Advanced Slider to Slider
    - add ko import

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/slide.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/slide.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-define(["./block"], function (_block) {
+define(["knockout", "./block"], function (_knockout, _block) {
   function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
   function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
@@ -18,7 +18,7 @@ define(["./block"], function (_block) {
 
       _this = _PreviewBlock.call(this, parent, config) || this;
       _this.backgroundImageStyle = void 0;
-      _this.backgroundImageStyle = ko.computed(function () {
+      _this.backgroundImageStyle = _knockout.computed(function () {
         if (_this.data.background_image && _typeof(_this.data.background_image()[0]) === "object") {
           return {
             backgroundImage: "url(" + _this.data.background_image()[0].url + ")"
