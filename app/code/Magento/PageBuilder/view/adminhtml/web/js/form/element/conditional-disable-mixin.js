de023353004d7a2cb3b741df09441acb8dd2f103
Krissy Hiserote <khiserote@magento.com>
2018-02-14 15:17:16 -0600

    MAGETWO-87812: Dependent Visibility of UI Component Form Field
    - disable fields and don't show button when never_show is selected

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/form/element/conditional-disable-mixin.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'underscore'
+], function (_) {
+    'use strict';
+
+    var setDisabledSetting = {
+        defaults: {
+            disabledValues: []
+        },
+
+        /**
+         * Set the field as disabled when it's empty or is equal to a value within disabledValues
+         *
+         * @param value
+         */
+        setDisabled: function (value) {
+            if (_.indexOf(_.values(this.disabledValues), value) !== -1) {
+                this.disable();
+            } else {
+                this.enable();
+            }
+        }
+    };
+
+    return function (target) {
+        return target.extend(setDisabledSetting);
+    };
+});
