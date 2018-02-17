158fd146111141638f64c89a9e4fc35f9dfb49be
Tommy Wiebell <twiebell@magento.com>
2018-02-16 20:37:07 -0600

    MAGETWO-87805: Automatically Add Placeholder Button
    - Fix types in BlockFactory and Buttons

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/buttons.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/buttons.ts
@@ -7,7 +7,7 @@ import delayedPromise from "../../utils/delayed-promise";
 import createBlock from "../block/factory";
 import Config from "../config";
 import Block from "./block";
-import ButtomItemBlock from "./button-item";
+import { Block as BlockInterface } from "./block.d";
 
 export default class Buttons extends Block {
 
@@ -15,17 +15,17 @@ export default class Buttons extends Block {
      * Add button-item to buttons children array
      */
     public addButton() {
-        const createBlockPromise: Promise<ButtomItemBlock> = createBlock(
+        const createBlockPromise: Promise<BlockInterface> = createBlock(
             Config.getInitConfig("content_types")["button-item"],
             this.parent,
             this.stage,
             {},
         );
 
-        createBlockPromise.then((button: ButtomItemBlock) => {
+        createBlockPromise.then((button: BlockInterface) => {
             this.addChild(button);
             return button;
-        }).then(delayedPromise(300)).then((button: ButtomItemBlock) => {
+        }).then(delayedPromise(300)).then((button: BlockInterface) => {
             button.edit.open();
         }).catch((error: Error) => {
             console.error(error);
