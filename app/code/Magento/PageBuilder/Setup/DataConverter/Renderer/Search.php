8089cc1b30b914a0497901d2354a04030b0e8231
Robert He <rohe@magento.com>
2018-03-06 17:21:27 -0600

    MAGETWO-88561: Removal of content block
     - removed the 'Search' content block

--- app/code/Magento/PageBuilder/Setup/DataConverter/Renderer/Search.php
+++ app/code/Magento/PageBuilder/Setup/DataConverter/Renderer/Search.php
@@ -24,33 +24,12 @@ class Search implements RendererInterface
      */
     private $eavAttributeLoader;
 
-    /**
-     * @var \Magento\Framework\Math\Random
-     */
-    private $mathRandom;
-
-    /**
-     * @var \Magento\Search\Helper\Data
-     */
-    private $searchHelper;
-
-    /**
-     * @var \Magento\Cms\Block\Block
-     */
-    private $block;
-
     public function __construct(
         StyleExtractorInterface $styleExtractor,
-        EavAttributeLoaderInterface $eavAttributeLoader,
-        \Magento\Framework\Math\Random $mathRandom,
-        \Magento\Search\Helper\Data $searchHelper,
-        \Magento\Cms\Block\Block $block
+        EavAttributeLoaderInterface $eavAttributeLoader
     ) {
         $this->styleExtractor = $styleExtractor;
         $this->eavAttributeLoader = $eavAttributeLoader;
-        $this->mathRandom = $mathRandom;
-        $this->searchHelper = $searchHelper;
-        $this->block = $block;
     }
 
     /**
@@ -63,49 +42,23 @@ class Search implements RendererInterface
         }
         $eavData = $this->eavAttributeLoader->load($itemData['entityId']);
 
-        // Search content it migrated as HTML content block
-        $rootElementAttributes = [
-            'data-role' => 'html',
-        ];
-
-        $searchStyle = '';
+        $searchStyles = '';
         if (isset($itemData['formData'])) {
             $style = $this->styleExtractor->extractStyle($itemData['formData']);
             if ($style) {
-                $searchStyle = $style;
+                $searchStyles = $style;
             }
         }
 
-        $uniqueIdentifier = $this->mathRandom::getRandomNumber();
         $searchClasses = $eavData['css_classes'] ?? '';
         $searchPlaceHolder = $eavData['placeholder'] ?? '';
 
-        $searchHtml = <<<SEARCH
-<form id="search_mini_form_$uniqueIdentifier" 
-class="pagebuilder-search-form pagebuilder-full-width $searchClasses" 
-style="$searchStyle" 
-action="{$this->searchHelper->getResultUrl()}" 
-method="get">
-<div class="form-search">
-<label for="search_$uniqueIdentifier">{$this->block->escapeHtml(__('Search:'))}</label>
-<input id="search_$uniqueIdentifier" type="search" 
-name="{$this->block->escapeHtml($this->searchHelper->getQueryParamName())}" 
-value="{$this->block->escapeHtml($this->searchHelper->getEscapedQueryText())}" class="input-text" 
-maxlength="{$this->block->escapeHtml($this->searchHelper->getMaxQueryLength())}" 
-placeholder="{$this->block->escapeHtml($searchPlaceHolder)}"/>
-<button type="submit" title="{$this->block->escapeHtmlAttr(__('Search'))}" class="button search-button"></button>
-<div id="search_autocomplete_$uniqueIdentifier" class="search-autocomplete"></div>
-</div>
-</form>
-SEARCH;
+        $searchHtml = "{{block class='Magento\PageBuilder\Block\ContentType\Search' " .
+            "template='Magento_PageBuilder::pagebuilder/blocks/core/search.phtml' " .
+            "placeholder=\"$searchPlaceHolder\" classes=\"$searchClasses\" styles=\"$searchStyles\"}}";
+        $rootElementHtml = '<div data-role="html">';
 
-        $searchHtml = str_replace(PHP_EOL,'', $searchHtml);
-        $rootElementHtml = '<div';
-        foreach ($rootElementAttributes as $attributeName => $attributeValue) {
-            $rootElementHtml .= $attributeValue ? " $attributeName=\"$attributeValue\"" : '';
-        }
-
-        $rootElementHtml .= '>' . $searchHtml . '</div>';
+        $rootElementHtml .= $searchHtml . '</div>';
 
         return $rootElementHtml;
     }
