431daa4ab1779f71f2ddce8b0cf2272c878e0bee
Igor Melnikov <imelnikov@magento.com>
2018-03-20 21:16:50 -0500

    MAGETWO-88956: Refactor row to use new configuration
    - remove preview component

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/converter/row/margins-and-paddings.js
@@ -0,0 +1,79 @@
+/*eslint-disable */
+define([], function () {
+  /**
+   * Copyright © Magento, Inc. All rights reserved.
+   * See COPYING.txt for license details.
+   */
+  var MarginsAndPaddings =
+  /*#__PURE__*/
+  function () {
+    function MarginsAndPaddings() {}
+
+    var _proto = MarginsAndPaddings.prototype;
+
+    /**
+     * @param {string} value
+     * @returns {Object | string}
+     */
+    _proto.fromDom = function fromDom(value) {
+      var result = {};
+
+      if (undefined !== value.margin) {
+        result["margin"] = {
+          "left": value.margin.left.replace("px", ""),
+          "top": value.margin.top.replace("px", ""),
+          "right": value.margin.right.replace("px", ""),
+          "bottom": value.margin.bottom.replace("px", "")
+        };
+      }
+
+      if (undefined !== value.padding) {
+        result["padding"] = {
+          "left": value.padding.left.replace("px", ""),
+          "top": value.padding.top.replace("px", ""),
+          "right": value.padding.right.replace("px", ""),
+          "bottom": value.padding.bottom.replace("px", "")
+        };
+      }
+
+      return result;
+    };
+    /**
+     * @param {string} name
+     * @param {Object} data
+     * @returns {Object | string}
+     */
+
+
+    _proto.toDom = function toDom(name, data) {
+      var value = data[name];
+
+      if (typeof value === "string") {
+        value = JSON.parse(value);
+      }
+
+      var result = {};
+
+      if (undefined !== value && undefined !== value.margin) {
+        result["marginLeft"] = value.margin.left + "px";
+        result["marginTop"] = value.margin.top + "px";
+        result["marginRight"] = value.margin.right + "px";
+        result["marginBottom"] = value.margin.bottom + "px";
+      }
+
+      if (undefined !== value && undefined !== value.padding) {
+        result["paddingLeft"] = value.padding.left + "px";
+        result["paddingTop"] = value.padding.top + "px";
+        result["paddingRight"] = value.padding.right + "px";
+        result["paddingBottom"] = value.padding.bottom + "px";
+      }
+
+      return result;
+    };
+
+    return MarginsAndPaddings;
+  }();
+
+  return MarginsAndPaddings;
+});
+//# sourceMappingURL=margins-and-paddings.js.map
---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/converter/row/style/margins-and-paddings.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import ElementConverterInterface from "../../element-converter-interface";
+
+export default class MarginsAndPaddings implements ElementConverterInterface {
+    /**
+     * @param {string} value
+     * @returns {Object | string}
+     */
+    public fromDom(value: string): string | object {
+        return value;
+    }
+
+    /**
+     * @param {string} name
+     * @param {Object} data
+     * @returns {Object | string}
+     */
+    public toDom(name: string, data:object): string | object {
+        let value = data[name];
+        if (typeof value === "string") {
+            value = JSON.parse(value);
+        }
+        const result = {};
+        if (undefined !== value && undefined !== value.margin) {
+            result["marginLeft"] = value.margin.left + "px";
+            result["marginTop"] = value.margin.top + "px";
+            result["marginRight"] = value.margin.right + "px";
+            result["marginBottom"] = (value.margin.bottom !== "0" ? value.margin.bottom : 1) + "px";
+        }
+        if (undefined !== value && undefined !== value.padding) {
+            result["paddingLeft"] = value.padding.left + "px";
+            result["paddingTop"] = value.padding.top + "px";
+            result["paddingRight"] = value.padding.right + "px";
+            result["paddingBottom"] = value.padding.bottom + "px";
+        }
+        return result;
+    }
+}
