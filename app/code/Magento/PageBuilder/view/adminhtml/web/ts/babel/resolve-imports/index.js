84d752129398cc45cb56f59a25fd5a6df7ef97bd
Dave Macaulay <dmacaulay@magento.com>
2018-03-23 14:55:52 +0100

    MAGETWO-86866: [Tech Debt] Allow extension of PageBuilder functionality via mixins
    - Use node path library for path manipulations

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/babel/resolve-imports/index.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/babel/resolve-imports/index.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 /**
  * Convert the relative path imports within TypeScript into absolute paths with pre-fixed module name
  *
@@ -11,39 +13,26 @@ module.exports = function () {
             /**
              * Convert ../../utils/util import into Magento_Module/js/utils/util
              *
-             * @param path
+             * @param importPath
              * @param state
              * @constructor
              */
-            ImportDeclaration: function (path, state) {
-                var prefix, fileDir, importParts, traverseUpwards, fileDirParts, absolutePath, importPath;
-                var fileName = state.file.opts.filenameRelative;
-                var importExpression = path.node.source.value;
+            ImportDeclaration: function (importPath, state) {
+                var importExpression = importPath.node.source.value;
 
                 if (!state.opts.prefix) {
                     throw Error("Prefix must be defined");
                 }
 
-                // Config
-                prefix = state.opts.prefix;
-
                 // Is the file being imported from another directory?
-                if (fileName && importExpression.indexOf("./") !== -1) {
-                    fileDir = fileName.substr(0, fileName.lastIndexOf("/"));
-                    importParts = importExpression.split("/");
-                    traverseUpwards = 0;
-                    importParts.forEach(function (part) {
-                        if (part === "..") {
-                            ++traverseUpwards;
-                        }
-                    });
-
-                    fileDirParts = fileDir.split("/");
-                    absolutePath = fileDirParts.slice(0, fileDirParts.length - traverseUpwards).join("/") + "/";
-                    importPath = importParts.slice(traverseUpwards).join("/").replace("./", "");
-
-                    // Translated path
-                    path.node.source.value = prefix + absolutePath + importPath;
+                if (!path.isAbsolute(importExpression) && importExpression.includes("./")) {
+                    importPath.node.source.value = path.resolve(
+                        path.dirname(state.file.opts.filenameRelative),
+                        importExpression
+                    ).replace(
+                        path.resolve(),
+                        state.opts.prefix.replace(/\/+$/, '')
+                    );
                 }
             }
         }
