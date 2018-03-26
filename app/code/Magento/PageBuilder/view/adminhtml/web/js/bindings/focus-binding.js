64e3d10f9dc0531d21015cfea5e17302fa368063
Dave Macaulay <dmacaulay@magento.com>
2018-03-26 11:49:47 +0200

    MAGETWO-89103: Move slide options panel for slide into navigation for slider
    - Move focus binding into dedicated folder

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/bindings/focus-binding.js
@@ -0,0 +1,23 @@
+/*eslint-disable */
+define(["knockout"], function (_knockout) {
+  "use strict";
+
+  _knockout = _interopRequireDefault(_knockout);
+
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+  /**
+   * Copyright © Magento, Inc. All rights reserved.
+   * See COPYING.txt for license details.
+   */
+  _knockout.default.bindingHandlers.hasFocusNoScroll = {
+    init: _knockout.default.bindingHandlers.hasFocus.init,
+    update: function update(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+      var value = !!_knockout.default.utils.unwrapObservable(valueAccessor());
+      value ? element.focus({
+        preventScroll: true
+      }) : element.blur();
+    }
+  };
+});
+//# sourceMappingURL=focus-binding.js.map
