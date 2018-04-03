6ab14700574843904c139bffa3e0f8f3401e5312
Michael Yu <hyu@magento.com>
2018-04-03 11:26:27 -0500

    MAGETWO-89720: Data seperation for product list component - Updated to have ajax request to use eventbus instead of callback function

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/product-list.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/product-list.ts
@@ -4,26 +4,34 @@
  */
 
 import Config from "../config";
+import EventBus from "../event-bus";
 import Block from "./block";
 
 export default class ProductList extends Block {
 
-    protected afterDataRendered() {
-        const attributes = this.data.main.attributes();
-        if (attributes["data-category-id"] === "") {
-            return;
-        }
-        const url = Config.getInitConfig("preview_url");
-        const requestData = {
-            is_preview: true,
-            role: this.config.name,
-            category_id: attributes["data-category-id"],
-            hide_out_of_stock: attributes["data-hide-out-of-stock"],
-            product_count: attributes["data-product-count"],
-        };
+    /**
+     * Bind events for the current instance
+     */
+    protected bindEvents() {
+        super.bindEvents();
 
-        jQuery.post(url, requestData, (response) => {
-            this.data.main.html(response.content !== undefined ? response.content.trim() : "");
+        EventBus.on("previewObservables:updated", (event, params) => {
+            const attributes = this.data.main.attributes();
+            if (attributes["data-category-id"] === "") {
+                return;
+            }
+            const url = Config.getInitConfig("preview_url");
+            const requestData = {
+                is_preview: true,
+                role: this.config.name,
+                category_id: attributes["data-category-id"],
+                hide_out_of_stock: attributes["data-hide-out-of-stock"],
+                product_count: attributes["data-product-count"],
+            };
+
+            jQuery.post(url, requestData, (response) => {
+                this.data.main.html(response.content !== undefined ? response.content.trim() : "");
+            });
         });
     }
 }
