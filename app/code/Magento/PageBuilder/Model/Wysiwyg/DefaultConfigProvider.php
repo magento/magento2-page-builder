c3843cd24b6a8cc5fbe36a19d87fe945d487f7a4
Robert He <rohe@magento.com>
2018-04-12 17:50:38 -0500

    MAGETWO-90209: Add luma styles to wysiwyg
     - added styles to editor and pagebuilder

--- app/code/Magento/PageBuilder/Model/Wysiwyg/DefaultConfigProvider.php
+++ app/code/Magento/PageBuilder/Model/Wysiwyg/DefaultConfigProvider.php
@@ -55,21 +55,52 @@ class DefaultConfigProvider implements \Magento\Framework\Data\Wysiwyg\ConfigPro
                         'image'
                     ]
                 ),
-                'content_css' => $this->assetRepo->getUrl('mage/adminhtml/wysiwyg/tiny_mce/themes/ui.css')
+                'content_css' => [
+                    $this->assetRepo->getUrl('mage/adminhtml/wysiwyg/tiny_mce/themes/ui.css'),
+                    $this->assetRepo->getUrl('Magento_PageBuilder/css/source/wysiwyg/tinymce.css')
+                ]
             ],
             'settings' => [
                 'fontsize_formats' => '10px 12px 14px 16px 18px 20px 24px 26px 28px 32px 34px 36px 38px 40px 42px ' .
                     '48px 52px 56px 64px 72px',
                 'style_formats' => [
-                    ['title' => 'Paragraph', 'block' => 'p'],
-                    ['title' => 'Heading 1', 'block' => 'h1'],
-                    ['title' => 'Heading 2', 'block' => 'h2'],
-                    ['title' => 'Heading 3', 'block' => 'h3'],
-                    ['title' => 'Heading 4', 'block' => 'h4'],
-                    ['title' => 'Heading 5', 'block' => 'h5'],
-                    ['title' => 'Heading 6', 'block' => 'h6'],
-                    ['title' => 'Important', 'block' => 'p', 'classes' => 'cms-content-important'],
-                    ['title' => 'Preformatted', 'block' => 'pre']
+                    [
+                        'title' => 'Paragraph',
+                        'block' => 'p'
+                    ],
+                    [
+                        'title' => 'Heading 1',
+                        'block' => 'h1'
+                    ],
+                    [
+                        'title' => 'Heading 2',
+                        'block' => 'h2'
+                    ],
+                    [
+                        'title' => 'Heading 3',
+                        'block' => 'h3'
+                    ],
+                    [
+                        'title' => 'Heading 4',
+                        'block' => 'h4'
+                    ],
+                    [
+                        'title' => 'Heading 5',
+                        'block' => 'h5'
+                    ],
+                    [
+                        'title' => 'Heading 6',
+                        'block' => 'h6'
+                    ],
+                    [
+                        'title' => 'Important',
+                        'block' => 'p',
+                        'classes' => 'cms-content-important'
+                    ],
+                    [
+                        'title' => 'Preformatted',
+                        'block' => 'pre'
+                    ]
                 ]
             ]
         ]);
