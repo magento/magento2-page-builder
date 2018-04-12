ee2b48c5b8931147d3c8ec52fdddaab21ff03ed6
Krissy Hiserote <khiserote@magento.com>
2018-04-12 11:53:48 -0500

    MAGETWO-90251: Troubleshoot and fix row content type
    - remove unused files

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/component/block/row.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-import ContentTypeCollection from "../../content-type-collection";
-
-export default class Row extends ContentTypeCollection {
-}
