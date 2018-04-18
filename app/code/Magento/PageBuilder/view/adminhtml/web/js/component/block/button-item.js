47577d4e4a379299007ce4e558f5298b8bfd9ca5
Michael Yu <hyu@magento.com>
2018-04-18 10:16:23 -0500

    MAGETWO-90256: Troubleshoot and fix buttons content type
    - Moved component logic to preview and content component

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/button-item.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/button-item.js
@@ -1,7 +1,5 @@
 /*eslint-disable */
-define(["underscore", "Magento_PageBuilder/js/content-type"], function (_underscore, _contentType) {
-  function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
+define(["Magento_PageBuilder/js/content-type"], function (_contentType) {
   function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
   var ButtonItem =
@@ -12,89 +10,6 @@ define(["underscore", "Magento_PageBuilder/js/content-type"], function (_undersc
     function ButtonItem() {
       return _ContentType.apply(this, arguments) || this;
     }
-
-    var _proto = ButtonItem.prototype;
-
-    /**
-     * Get the attributes for link
-     * returns {object}
-     */
-    _proto.getLinkAttributes = function getLinkAttributes() {
-      var data = this.content.getData();
-
-      if (_typeof(data.button_link) === "object") {
-        var href = data.button_link[data.button_link.type];
-
-        switch (data.button_link.type) {
-          case "category":
-            href = this.convertToCategoryWidget(href);
-            break;
-
-          case "product":
-            href = this.convertToProductWidget(href);
-            break;
-
-          case "default":
-            break;
-        }
-
-        return {
-          data_attribute_link_type: data.button_link.type,
-          href: href,
-          target: data.button_link.setting === true ? "_blank" : ""
-        };
-      } else {
-        return {
-          data_attribute_link_type: "",
-          href: "",
-          target: ""
-        };
-      }
-    };
-    /**
-     * Convert plain href string to category widget string
-     *
-     * @param {string} href
-     * @returns {string}
-     */
-
-
-    _proto.convertToCategoryWidget = function convertToCategoryWidget(href) {
-      var attributes = {
-        type: "Magento\\Catalog\\Block\\Category\\Widget\\Link",
-        id_path: "category/" + href,
-        template: "category/widget/link/link_href.phtml",
-        type_name: "Catalog Category Link"
-      };
-
-      var attributesString = _underscore.map(attributes, function (val, key) {
-        return key + "='" + val + "'";
-      }).join(" ");
-
-      return "{{widget " + attributesString + " }}";
-    };
-    /**
-     * Convert plain href string to product widget string
-     *
-     * @param {string} href
-     * @returns {string}
-     */
-
-
-    _proto.convertToProductWidget = function convertToProductWidget(href) {
-      var attributes = {
-        type: "Magento\\Catalog\\Block\\Product\\Widget\\Link",
-        id_path: "product/" + href,
-        template: "category/widget/link/link_href.phtml",
-        type_name: "Catalog Product Link"
-      };
-
-      var attributesString = _underscore.map(attributes, function (val, key) {
-        return key + "='" + val + "'";
-      }).join(" ");
-
-      return "{{widget " + attributesString + " }}";
-    };
 
     return ButtonItem;
   }(_contentType);
--- app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/buttons.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/component/block/preview/buttons.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-define(["knockout", "Magento_PageBuilder/js/preview-collection"], function (_knockout, _previewCollection) {
+define(["knockout", "Magento_PageBuilder/js/preview-collection", "Magento_PageBuilder/js/component/block/factory", "Magento_PageBuilder/js/component/config", "Magento_PageBuilder/js/component/event-bus"], function (_knockout, _previewCollection, _factory, _config, _eventBus) {
   function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
 
   var Buttons =
@@ -16,6 +16,39 @@ define(["knockout", "Magento_PageBuilder/js/preview-collection"], function (_kno
 
       return (_temp = _this = _PreviewCollection.call.apply(_PreviewCollection, [this].concat(args)) || this, _this.isLiveEditing = _knockout.observable(false), _temp) || _this;
     }
+
+    var _proto = Buttons.prototype;
+
+    _proto.bindEvents = function bindEvents() {
+      var _this2 = this;
+
+      _PreviewCollection.prototype.bindEvents.call(this);
+
+      _eventBus.on("buttons:block:ready", function (event, params) {
+        if (params.id === _this2.parent.id && _this2.parent.children().length === 0) {
+          _this2.addButton();
+        }
+      });
+    };
+    /**
+     * Add button-item to buttons children array
+     */
+
+
+    _proto.addButton = function addButton() {
+      var _this3 = this;
+
+      var createBlockPromise = (0, _factory)(_config.getConfig("content_types")["button-item"], this.parent, this.parent.stageId, {});
+      createBlockPromise.then(function (button) {
+        _this3.parent.addChild(button);
+
+        _this3.isLiveEditing(_this3.parent.children().indexOf(button));
+
+        return button;
+      }).catch(function (error) {
+        console.error(error);
+      });
+    };
 
     return Buttons;
   }(_previewCollection);
