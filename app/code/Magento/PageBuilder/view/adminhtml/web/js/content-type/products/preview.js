40df7705d670ddbbb125d058cab40f4933eaaea4
Nathan Smith <nathanjosiah@gmail.com>
2018-05-01 15:09:47 -0500

    MAGETWO-71425: Product list configuration with Magento Condition Tree
    - Merged Product and Product List content types into Products
    - Removed some product list options and replaced with conditions rule tree filter
    - Styled rendered products in WYSIWYG

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/content-type/products/preview.js
@@ -0,0 +1,78 @@
+/*eslint-disable */
+define(["jquery", "knockout", "uiEvents", "Magento_PageBuilder/js/config", "Magento_PageBuilder/js/content-type/preview", "Magento_PageBuilder/js/utils/delayed-promise"], function (_jquery, _knockout, _uiEvents, _config, _preview, _delayedPromise) {
+  function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; subClass.__proto__ = superClass; }
+
+  var Preview =
+  /*#__PURE__*/
+  function (_BasePreview) {
+    _inheritsLoose(Preview, _BasePreview);
+
+    function Preview() {
+      var _temp, _this;
+
+      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      return (_temp = _this = _BasePreview.call.apply(_BasePreview, [this].concat(args)) || this, _this.displayPreview = _knockout.observable(false), _temp) || _this;
+    }
+
+    var _proto = Preview.prototype;
+
+    /**
+     * Bind events
+     */
+    _proto.bindEvents = function bindEvents() {
+      var _this2 = this;
+
+      _BasePreview.prototype.bindEvents.call(this); // When a products type is dropped for the first time open the edit panel
+
+
+      _uiEvents.on("products:block:dropped:create", function (event, params) {
+        if (event.id === _this2.parent.id) {
+          (0, _delayedPromise)(300)().then(function () {
+            _this2.edit.open();
+          });
+        }
+      });
+
+      _uiEvents.on("previewObservables:updated", function (event, params) {
+        if (event.preview.parent.id !== _this2.parent.id) {
+          return;
+        }
+
+        _this2.displayPreview(false);
+
+        var data = _this2.parent.dataStore.get();
+
+        if (typeof data.conditions_encoded !== "string" || data.conditions_encoded.length === 0) {
+          return;
+        }
+
+        var url = _config.getConfig("preview_url");
+
+        var requestData = {
+          role: _this2.config.name,
+          directive: _this2.data.main.html()
+        };
+
+        _jquery.post(url, requestData, function (response) {
+          var content = response.content !== undefined ? response.content.trim() : "";
+
+          if (content.length === 0) {
+            return;
+          }
+
+          _this2.data.main.html(content);
+
+          _this2.displayPreview(true);
+        });
+      });
+    };
+
+    return Preview;
+  }(_preview);
+
+  return Preview;
+});
+//# sourceMappingURL=preview.js.map
