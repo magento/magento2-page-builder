e2dac5b0ddad89f4a582baf114d27ac852a778c9
Dan Mooney <dmooney@magento.com>
2018-05-16 12:26:41 -0500

    MAGETWO-91447: Appearance section shouldn't be collapsible
    Uniquely style appearance fieldset and add delegate import to hide label only if other elements exist in fieldset

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/form/element/dependent-fieldset.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/form/element/dependent-fieldset.js
@@ -9,13 +9,54 @@ define([
     'use strict';
 
     return Fieldset.extend({
+        defaults: {
+            originalLabelValue: ''
+        },
+
         /**
-         * Hide fieldset
+         * Observe label
+         * {@inheritDoc}
+         */
+        initObservable: function () {
+            this._super().observe('label');
+
+            return this;
+        },
+
+        /**
+         * Store original label value
+         * {@inheritDoc}
+         */
+        initConfig: function () {
+            this._super();
+            this.originalLabelValue = this.label;
+        },
+
+        /**
+         * Hide fieldset if appearance field options is <= 1
          *
          * @param {Array} options
          */
         appearancesHidden: function (options) {
             this.visible(options.length > 1);
+        },
+
+        /**
+         * Delegate element to show/hide based on number of elements in fieldset
+         *
+         * @param {Array} options
+         */
+        appearancesHiddenLabelOnlyIfOtherElementsExist: function (options) {
+            var otherElemsExist = this.elems().length > 1;
+
+            if (!otherElemsExist) { // show/hide fieldset
+                this.appearancesHidden(options);
+
+                return;
+            }
+
+            // show/hide label
+            this.label(options.length > 1 ? this.originalLabelValue : '');
         }
     });
 });
