6229c503ee0610535a88bd2bcc254d83025b0c77
Iryna Lagno <ilagno@magento.com>
2018-05-17 13:40:54 -0500

    MAGETWO-91241: Build stabilization for Product list configuration with Magento Condition Tree

--- dev/tests/integration/testsuite/Magento/PageBuilder/Controller/Adminhtml/Products/Conditions/ChildTest.php
+++ dev/tests/integration/testsuite/Magento/PageBuilder/Controller/Adminhtml/Products/Conditions/ChildTest.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 namespace Magento\PageBuilder\Controller\Adminhtml\Products\Conditions;
 
 class ChildTest extends \Magento\TestFramework\TestCase\AbstractBackendController
