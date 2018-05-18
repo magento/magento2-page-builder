263981d70495592d30d85e36852c0dc5238bb78b
Michael Yu <hyu@magento.com>
2018-05-17 19:26:47 -0500

    MAGETWO-91206: Disclaimer when key is not valide on map configuration
    - Abastracted key validation out to seperate class
    - Created map container class to allow rendering of full url for backend config

---
+++ app/code/Magento/PageBuilder/Component/MapContainer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\PageBuilder\Component;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentInterface;
+
+class MapContainer extends \Magento\Ui\Component\Container
+{
+    /**
+     * @var UrlInterface $url
+     */
+    private $url;
+    private $scopeConfig;
+    private $keyValidator;
+
+    const GOOGLE_MAPS_API_KEY_PATH = 'cms/pagebuilder/google_maps_api_key';
+
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentInterface[] $components
+     * @param array $data
+     * @param UrlInterface $url
+     * @param ScopeConfigInterface $scopeConfig
+     * @param \Magento\PageBuilder\Model\GoogleMaps\KeyValidator $keyValidator
+     */
+    public function __construct(
+        ContextInterface $context,
+        UrlInterface $url,
+        ScopeConfigInterface $scopeConfig,
+        \Magento\PageBuilder\Model\GoogleMaps\KeyValidator $keyValidator,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $components,
+            $data
+        );
+        $this->url = $url;
+        $this->scopeConfig = $scopeConfig;
+        $this->keyValidator = $keyValidator;
+    }
+
+    public function prepare()
+    {
+        parent::prepare();
+        $config = $this->getData('config');
+        $apiKey = $this->scopeConfig->getValue(self::GOOGLE_MAPS_API_KEY_PATH);
+        $response = $this->keyValidator->validate($apiKey);
+        if (!$response["success"]) {
+            $config['visible'] = true;
+        }
+
+        if (isset($config['map_configuration_url'])) {
+            $config['map_configuration_url'] = $this->url->getUrl($config['map_configuration_url']);
+        }
+        if (isset($config['content'])) {
+            $config['content'] = sprintf($config['content'], $config['map_configuration_url']);
+        }
+
+        $this->setData('config', (array) $config);
+    }
+}
--- app/code/Magento/PageBuilder/Controller/Adminhtml/GoogleMaps/ValidateApi.php
+++ app/code/Magento/PageBuilder/Controller/Adminhtml/GoogleMaps/ValidateApi.php
@@ -11,46 +11,38 @@ class ValidateApi extends \Magento\Backend\App\Action
 {
     const ADMIN_RESOURCE = 'Magento_Backend::content';
 
-    const GOOGLE_MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/place?key=%s&q=Austin+TX';
-
     /**
      * @var \Magento\Framework\Controller\Result\JsonFactory
+     * @var \Magento\PageBuilder\Model\GoogleMaps\KeyValidator
      */
     private $resultJsonFactory;
+    private $keyValidator;
 
     /**
      * Constructor
      *
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param \Magento\PageBuilder\Model\GoogleMaps\KeyValidator $keyValidator
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\PageBuilder\Model\GoogleMaps\KeyValidator $keyValidator
     ) {
         parent::__construct($context);
         $this->resultJsonFactory = $resultJsonFactory;
+        $this->keyValidator = $keyValidator;
     }
 
     /**
      * Send test request to Google Maps and return response
      *
-     * @return $this|\Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
         $apiKey = $this->getRequest()->getParam('key');
-        $testUrl = sprintf(self::GOOGLE_MAPS_EMBED_URL, $apiKey);
-        $curl = curl_init($testUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($curl);
-        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        $returnArray = [
-            'responseCode' => $responseCode,
-            'responseMessage' => $responseCode !== 200 ? $result : '',
-            'success' => $responseCode === 200 ? true : false
-        ];
-
-        return $this->resultJsonFactory->create()->setData($returnArray);
+        return $this->resultJsonFactory->create()->setData($this->keyValidator->validate($apiKey));
     }
 }
