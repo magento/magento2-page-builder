b0a85ce4e74c2bbb1824bc38577da00cb5f3d389
Dave Macaulay <dmacaulay@magento.com>
2018-05-22 12:29:18 +0200

    MAGETWO-91375: Refactor drag & drop code
    - Refactor create stylesheet logic

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/utils/create-stylesheet.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/utils/create-stylesheet.js
@@ -4,34 +4,70 @@ define([], function () {
    * Copyright © Magento, Inc. All rights reserved.
    * See COPYING.txt for license details.
    */
-  // @todo refactor this code
+
+  /**
+   * Create a stylesheet DOM object from a style block declaration
+   *
+   * @param {StyleBlocks} blocks
+   * @returns {HTMLStyleElement}
+   */
   function createStyleSheet(blocks) {
-    var style = document.createElement('style');
+    var style = document.createElement("style");
     var text = Object.keys(blocks).map(function (selector) {
       return processRuleSet(selector, blocks[selector]);
-    }).join('\n');
-    style.setAttribute('type', 'text/css');
+    }).join("\n");
+    style.setAttribute("type", "text/css");
     style.appendChild(document.createTextNode(text));
     return style;
   }
+  /**
+   * Process an individual rule set
+   *
+   * @param {string} selector
+   * @param {StyleBlock} block
+   * @returns {string}
+   */
+
 
   function processRuleSet(selector, block) {
-    return selector + ' {\n' + processDeclarationBlock(block) + '\n}';
+    return selector + (" {\n" + processDeclarationBlock(block) + "\n}");
   }
+  /**
+   * Process a declaration block from the rule set
+   *
+   * @param {StyleBlock} block
+   * @returns {string}
+   */
+
 
   function processDeclarationBlock(block) {
     return Object.keys(block).map(function (prop) {
       return processDeclaration(prop, block[prop]);
-    }).join('\n');
+    }).join("\n");
   }
+  /**
+   * Process a declaration, creating the property: value syntax
+   *
+   * @param {string} prop
+   * @param {string | number} value
+   * @returns {string}
+   */
+
 
   function processDeclaration(prop, value) {
-    return hyphenate(prop) + ': ' + value + ';';
+    return hyphenate(prop) + (": " + value + ";");
   }
+  /**
+   * Hyphenate style properties, from textAlign to text-align
+   *
+   * @param {string} prop
+   * @returns {string}
+   */
+
 
   function hyphenate(prop) {
     return prop.replace(/[A-Z]/g, function (match) {
-      return '-' + match.toLowerCase();
+      return "-" + match.toLowerCase();
     });
   }
 
--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/utils/create-stylesheet.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/utils/create-stylesheet.ts
@@ -3,35 +3,68 @@
  * See COPYING.txt for license details.
  */
 
-// @todo refactor this code
-export function createStyleSheet(blocks) {
-    var style = document.createElement('style');
-    var text = Object.keys(blocks).map(function (selector) {
-        return processRuleSet(selector, blocks[selector]);
-    }).join('\n');
+import {StyleBlock, StyleBlocks} from "./style-blocks";
 
-    style.setAttribute('type', 'text/css');
+/**
+ * Create a stylesheet DOM object from a style block declaration
+ *
+ * @param {StyleBlocks} blocks
+ * @returns {HTMLStyleElement}
+ */
+export function createStyleSheet(blocks: StyleBlocks) {
+    const style = document.createElement("style");
+    const text = Object.keys(blocks).map((selector) => {
+        return processRuleSet(selector, blocks[selector]);
+    }).join("\n");
+
+    style.setAttribute("type", "text/css");
     style.appendChild(document.createTextNode(text));
 
     return style;
 }
 
-function processRuleSet(selector, block) {
-    return selector + ' {\n' + processDeclarationBlock(block) + '\n}';
+/**
+ * Process an individual rule set
+ *
+ * @param {string} selector
+ * @param {StyleBlock} block
+ * @returns {string}
+ */
+function processRuleSet(selector: string, block: StyleBlock) {
+    return selector + ` {\n${processDeclarationBlock(block)}\n}`;
 }
 
-function processDeclarationBlock(block) {
-    return Object.keys(block).map(function (prop) {
+/**
+ * Process a declaration block from the rule set
+ *
+ * @param {StyleBlock} block
+ * @returns {string}
+ */
+function processDeclarationBlock(block: StyleBlock) {
+    return Object.keys(block).map((prop) => {
         return processDeclaration(prop, block[prop]);
-    }).join('\n');
+    }).join("\n");
 }
 
-function processDeclaration(prop, value) {
-    return hyphenate(prop) + ': ' + value + ';';
+/**
+ * Process a declaration, creating the property: value syntax
+ *
+ * @param {string} prop
+ * @param {string | number} value
+ * @returns {string}
+ */
+function processDeclaration(prop: string, value: string | number) {
+    return hyphenate(prop) + `: ${value};`;
 }
 
-function hyphenate(prop) {
-    return prop.replace(/[A-Z]/g, function (match) {
-        return '-' + match.toLowerCase();
+/**
+ * Hyphenate style properties, from textAlign to text-align
+ *
+ * @param {string} prop
+ * @returns {string}
+ */
+function hyphenate(prop: string) {
+    return prop.replace(/[A-Z]/g, (match) => {
+        return `-${match.toLowerCase()}`;
     });
 }
