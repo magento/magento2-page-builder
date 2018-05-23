805ae04158480b067bfefb27d28bd17e3c0c15dd
Michael Yu <hyu@magento.com>
2018-05-23 17:26:48 -0500

    MAGETWO-91979: Update tepates and controllers for Maps component
    - Removed hidden id field from location form
    - Updated dynamic rows to rely on record_id to grab the correct map marker for edit

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/form/components/dynamic-rows.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/form/components/dynamic-rows.js
@@ -49,7 +49,7 @@ define([
          */
         reInitData: function (data) {
             var recordData = this.recordData().slice(),
-                prop = this.identificationProperty,
+                prop = "record_id",
                 isExists = false;
 
             _.each(recordData, function (elem, index) {
