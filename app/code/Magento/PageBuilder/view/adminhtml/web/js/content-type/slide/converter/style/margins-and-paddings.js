911ec82a557b1fb1fe70be44af3d8a01ee69354a
Raith Hamzah <rhamzah@magento.com>
2018-06-01 12:38:52 -0500

    MAGETWO-89294: Slider Data Separation - Added custom margins and padding converter to handle reading from master format

---
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/content-type/slide/converter/style/margins-and-paddings.js
@@ -0,0 +1,92 @@
+/*eslint-disable */
+define([], function () {
+  /**
+   * Copyright © Magento, Inc. All rights reserved.
+   * See COPYING.txt for license details.
+   */
+  var MarginsAndPaddings =
+  /*#__PURE__*/
+  function () {
+    function MarginsAndPaddings() {}
+
+    var _proto = MarginsAndPaddings.prototype;
+
+    /**
+     * Convert value to internal format
+     *
+     * @param value string
+     * @returns {string | object}
+     */
+    _proto.fromDom = function fromDom(value) {
+      var result = {};
+
+      if (undefined !== value.margin) {
+        result.margin = {
+          bottom: value.margin.bottom.replace("px", ""),
+          left: value.margin.left.replace("px", ""),
+          right: value.margin.right.replace("px", ""),
+          top: value.margin.top.replace("px", "")
+        };
+      }
+
+      if (undefined !== value.padding) {
+        result.padding = {
+          bottom: value.padding.bottom.replace("px", ""),
+          left: value.padding.left.replace("px", ""),
+          right: value.padding.right.replace("px", ""),
+          top: value.padding.top.replace("px", "")
+        };
+      }
+
+      if (result.padding.right[0] === "c") {
+        result.padding.right = parseInt(result.padding.right.split("+")[1].trim(), 10);
+      }
+
+      if (result.padding.left[0] === "c") {
+        result.padding.left = parseInt(result.padding.left.split("+")[1].trim(), 10);
+      }
+
+      return result;
+    };
+    /**
+     * Convert value to knockout format
+     *
+     * @param name string
+     * @param data Object
+     * @returns {string | object}
+     */
+
+
+    _proto.toDom = function toDom(name, data) {
+      var result = {};
+      var value = data[name];
+
+      if (value && typeof value === "string") {
+        value = JSON.parse(value);
+      }
+
+      if (value) {
+        if (undefined !== value.margin) {
+          result.marginLeft = value.margin.left + "px";
+          result.marginTop = value.margin.top + "px";
+          result.marginRight = value.margin.right + "px";
+          result.marginBottom = value.margin.bottom + "px";
+        }
+
+        if (undefined !== value.padding) {
+          result.paddingLeft = value.padding.left + "px";
+          result.paddingTop = value.padding.top + "px";
+          result.paddingRight = value.padding.right + "px";
+          result.paddingBottom = value.padding.bottom + "px";
+        }
+      }
+
+      return result;
+    };
+
+    return MarginsAndPaddings;
+  }();
+
+  return MarginsAndPaddings;
+});
+//# sourceMappingURL=margins-and-paddings.js.map
