182bb1fdc60c1c00b64b256298d171103407a4b1
Iryna Lagno <ilagno@magento.com>
2018-06-13 16:51:56 -0500

    MAGETWO-92544: Video Enhancements: - build stabilization

--- dev/tests/js/jasmine/tests/app/code/PageBuilder/view/adminhtml/web/js/content-type/video/converter/style/text-align.test.js
+++ dev/tests/js/jasmine/tests/app/code/PageBuilder/view/adminhtml/web/js/content-type/video/converter/style/text-align.test.js
@@ -6,7 +6,8 @@
 /* eslint-disable max-nested-callbacks */
 define([
     'jquery',
-    'Magento_PageBuilder/js/content-type/video/converter/style/text-align'
+    'Magento_PageBuilder/js/content-type/video/converter/style/text-align',
+    'prototype'
 ], function ($, Converter) {
     'use strict';
 
@@ -19,25 +20,29 @@ define([
 
         describe('toDom', function () {
             it('Should pass through value when unknown', function () {
-                expect(model.toDom('foo')).toBe('foo');
+                var data = {
+                    'text-align': 'foo'
+                };
+
+                expect(model.toDom('text-align', data)).toBe('foo');
             });
             it('Should convert known values', function () {
-                expect(model.toDom('left')).toBe('flex-start');
-                expect(model.toDom('center')).toBe('center');
-                expect(model.toDom('right')).toBe('flext-end');
+                var data = {
+                    'text-align': 'left'
+                };
+
+                expect(model.toDom('text-align', data)).toBe('flex-start');
+                data['text-align'] = 'center';
+                expect(model.toDom('text-align', data)).toBe('center');
+                data['text-align'] = 'right';
+                expect(model.toDom('text-align', data)).toBe('flex-end');
             });
         });
         describe('fromDom', function () {
             it('Should convert known values', function () {
-                var data = {
-                    'text-align': 'flex-start'
-                };
-
-                expect(model.fromDom('text-align', data)).toBe('left');
-                data['text-align'] = 'center';
-                expect(model.fromDom('text-align', data)).toBe('center');
-                data['text-align'] = 'flex-end';
-                expect(model.fromDom('text-align', data)).toBe('right');
+                expect(model.fromDom('flex-start')).toBe('left');
+                expect(model.fromDom('center')).toBe('center');
+                expect(model.fromDom('flex-end')).toBe('right');
             });
         });
     });
