c6f5afc16acabbc389a8475b84d20125f3d3f7b1
Dave Macaulay <dmacaulay@magento.com>
2018-06-15 14:10:20 +0200

    MAGETWO-92821: [Tech Debt] Update slide & slider forms to use true / false values instead of 0 / 1
    - Ensure settings are handled correctly on the admin

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/content-type/slider/preview.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/content-type/slider/preview.ts
@@ -350,12 +350,12 @@ export default class Preview extends PreviewCollection {
     private buildSlickConfig() {
         const data = this.parent.dataStore.get();
         return {
-            arrows: data.show_arrows === "1",
-            autoplay: data.autoplay === "1",
+            arrows: data.show_arrows === "true",
+            autoplay: data.autoplay === "true",
             autoplaySpeed: data.autoplay_speed,
             dots: false, // We have our own dots implemented
-            fade: data.fade === "1",
-            infinite: data.is_infinite === "1",
+            fade: data.fade === "true",
+            infinite: data.is_infinite === "true",
             waitForAnimate: false,
         };
     }
