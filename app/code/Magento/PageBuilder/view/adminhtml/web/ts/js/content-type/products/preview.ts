e18fe8992bca407f752e809ad3f70390866a3665
Nathan Smith <nathanjosiah@gmail.com>
2018-06-25 12:16:08 -0500

    MC-1420: (Magento) Block Configuration
    - Removed extra space

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/content-type/products/preview.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/content-type/products/preview.ts
@@ -36,7 +36,6 @@ export default class Preview extends BasePreview {
             this.placeholderText($t("Loading..."));
             this.displayPreview(false);
 
-
             const data = this.parent.dataStore.get();
 
             if ((typeof data.conditions_encoded !== "string") || data.conditions_encoded.length === 0) {
