a85e88470c261521261fea1aa94d379fb890467a
Dave Macaulay <dmacaulay@magento.com>
2018-06-26 14:03:33 +0200

    MC-1172: Persist & validate grid size entry
    - Update alert interface name to contain interface at the end

--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/types/alert.d.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/types/alert.d.ts
@@ -3,12 +3,12 @@
  * See COPYING.txt for license details.
  */
 
-interface AlertConfig {
+interface AlertConfigInterface {
     content: string;
     title: string;
 }
 
-declare function Alert(config: AlertConfig): void;
+declare function Alert(config: AlertConfigInterface): void;
 
 declare module "Magento_Ui/js/modal/alert" {
     export = Alert;
