5fd462fd101cd8036730779aeea1b2e048b29d74
Dave Macaulay <dmacaulay@magento.com>
2018-07-16 12:35:30 +0200

    MC-3153: Apply full width to custom product attributes
    - Manipulate catalog product page via Page Layout to allow custom Page Builder attributes to be full width

---
+++ app/code/Magento/PageBuilder/Block/Catalog/Block/Product/View/Attributes.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\PageBuilder\Block\Catalog\Block\Product\View;
+
+class Attributes extends \Magento\Catalog\Block\Product\View\Attributes
+{
+    /**
+     * @inheritdoc
+     */
+    public function getAdditionalData(array $excludeAttr = [])
+    {
+        $data = [];
+        $product = $this->getProduct();
+        $attributes = $product->getAttributes();
+        foreach ($attributes as $attribute) {
+            if ($attribute->getIsVisibleOnFront() && !in_array($attribute->getAttributeCode(), $excludeAttr)) {
+                if ($this->getExcludePagebuilder() && $attribute->getIsPagebuilderEnabled()) {
+                    continue;
+                }
+                if ($this->getPagebuilderOnly() && !$attribute->getIsPagebuilderEnabled()) {
+                    continue;
+                }
+
+                $value = $attribute->getFrontend()->getValue($product);
+
+                if ($value instanceof Phrase) {
+                    $value = (string)$value;
+                } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
+                    $value = $this->priceCurrency->convertAndFormat($value);
+                }
+
+                if (is_string($value) && strlen($value)) {
+                    $data[$attribute->getAttributeCode()] = [
+                        'label' => __($attribute->getStoreLabel()),
+                        'value' => $value,
+                        'code' => $attribute->getAttributeCode(),
+                    ];
+                }
+            }
+        }
+        return $data;
+    }
+}
