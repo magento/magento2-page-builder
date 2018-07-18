487a9bcd9c985a663e534b5aea68bc6adf9ee37b
Dave Macaulay <dmacaulay@magento.com>
2018-07-18 18:34:21 +0200

    MC-3153: Apply full width to custom product attributes
    - Update to use new API introduced in CE

--- app/code/Magento/PageBuilder/Block/Catalog/Block/Product/View/Attributes.php
+++ app/code/Magento/PageBuilder/Block/Catalog/Block/Product/View/Attributes.php
@@ -10,40 +10,20 @@ namespace Magento\PageBuilder\Block\Catalog\Block\Product\View;
 class Attributes extends \Magento\Catalog\Block\Product\View\Attributes
 {
     /**
-     * @inheritdoc
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * Determine if we should display the attribute on the front-end, add support for exclude page builder & page
+     * builder only options on class.
+     *
+     * @param \Magento\Eav\Model\Entity\Attribute\AbstractAttribute $attribute
+     * @param array $excludeAttr
+     * @return bool
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
-    public function getAdditionalData(array $excludeAttr = [])
-    {
-        $data = [];
-        $product = $this->getProduct();
-        $attributes = $product->getAttributes();
-        foreach ($attributes as $attribute) {
-            if ($attribute->getIsVisibleOnFront() && !in_array($attribute->getAttributeCode(), $excludeAttr)) {
-                if ($this->getExcludePagebuilder() && $attribute->getIsPagebuilderEnabled()) {
-                    continue;
-                }
-                if ($this->getPagebuilderOnly() && !$attribute->getIsPagebuilderEnabled()) {
-                    continue;
-                }
-
-                $value = $attribute->getFrontend()->getValue($product);
-
-                if ($value instanceof Phrase) {
-                    $value = (string)$value;
-                } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
-                    $value = $this->priceCurrency->convertAndFormat($value);
-                }
-
-                if (is_string($value) && strlen($value)) {
-                    $data[$attribute->getAttributeCode()] = [
-                        'label' => __($attribute->getStoreLabel()),
-                        'value' => $value,
-                        'code' => $attribute->getAttributeCode(),
-                    ];
-                }
-            }
-        }
-        return $data;
+    protected function shouldDisplay(
+        \Magento\Eav\Model\Entity\Attribute\AbstractAttribute $attribute,
+        array $excludeAttr
+    ) {
+        return parent::shouldDisplay($attribute, $excludeAttr)
+            && (($this->getExcludePagebuilder() && !$attribute->getIsPagebuilderEnabled())
+            || ($this->getPagebuilderOnly() && $attribute->getIsPagebuilderEnabled()));
     }
 }
