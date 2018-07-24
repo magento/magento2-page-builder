5b3f99e49f56202d0bb5378587eeb17c7350a999
Michael Yu <hyu@magento.com>
2018-07-24 14:16:36 -0500

    MC-3363: Additional configuration node changes
    - Resolved static test failure
    - Resolved failed expected integration test results

--- dev/tests/integration/testsuite/Magento/PageBuilder/_files/content_type/expected_merged_array.php
+++ dev/tests/integration/testsuite/Magento/PageBuilder/_files/content_type/expected_merged_array.php
@@ -4,23 +4,25 @@
  * See COPYING.txt for license details.
  */
 return [
-    'group1' => [
-        'label' => 'Group 1',
-        'sortOrder' => '1',
-        'name' => 'group1',
-        'translate' => 'label'
-    ],
-    'group2' => [
-        'label' => 'Group 2 Label',
-        'sortOrder' => '3',
-        'name' => 'group2',
-        'translate' => 'label'
-    ],
-    'group3' => [
-        'label' => 'Group 3 Label',
-        'sortOrder' => '2',
-        'name' => 'group3',
-        'translate' => 'label'
+    'groups' => [
+        'group1' => [
+            'label' => 'Group 1',
+            'sortOrder' => '1',
+            'name' => 'group1',
+            'translate' => 'label'
+        ],
+        'group2' => [
+            'label' => 'Group 2 Label',
+            'sortOrder' => '3',
+            'name' => 'group2',
+            'translate' => 'label'
+        ],
+        'group3' => [
+            'label' => 'Group 3 Label',
+            'sortOrder' => '2',
+            'name' => 'group3',
+            'translate' => 'label'
+        ]
     ],
     'types' => [
         'type1' => [
