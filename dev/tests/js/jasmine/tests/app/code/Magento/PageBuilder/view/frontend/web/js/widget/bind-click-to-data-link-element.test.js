e455201f618dba1bfbdb3d2e9991c3235f6a5601
Dan Mooney <dmooney@adobe.com>
2018-09-24 11:05:49 -0500

    MC-3934: Slide/Banner Overlay & Button Do Not Show On Storefront When Set To On Hover
    Write JSUnit test for bindClickWidget

---
+++ dev/tests/js/jasmine/tests/app/code/Magento/PageBuilder/view/frontend/web/js/widget/bind-click-to-data-link-element.test.js
@@ -0,0 +1,122 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'Magento_PageBuilder/js/widget/bind-click-to-data-link-element',
+    'jquery'
+], function (bindClickToLinkInitializerWidget, $) {
+    'use strict';
+
+    var $el;
+
+    afterEach(function () {
+        if ($el !== undefined) {
+            $el.remove();
+        }
+    });
+
+    describe('Magento_PageBuilder/js/widget/bind-click-to-data-link-element', function () {
+        it('Should not navigate away from page if it is missing href attribute', function () {
+            spyOn(bindClickToLinkInitializerWidget, 'redirectTo');
+            $el = $(
+                '<div data-role="content-type">' +
+                    '<div data-link-type="link" href="">' +
+                        '<span class="span-outside-inner-anchor">Hello world</span>' +
+                        '<a class="inner-anchor" href="https://something.com"><span>Something</span></a>' +
+                    '</div>' +
+                '</div>'
+            );
+
+            $el.appendTo('body');
+
+            bindClickToLinkInitializerWidget(null, $el);
+
+            $el.find('.span-outside-inner-anchor').click();
+
+            expect(bindClickToLinkInitializerWidget.redirectTo).not.toHaveBeenCalled();
+        });
+
+        it('Should not navigate away from page if href is javascript:void(0)', function () {
+            spyOn(bindClickToLinkInitializerWidget, 'redirectTo');
+            $el = $(
+                '<div data-role="content-type">' +
+                    '<div data-link-type="link" href="javascript:void(0)">' +
+                        '<span class="span-outside-inner-anchor">Hello world</span>' +
+                        '<a class="inner-anchor" href="https://something.com"><span>Something</span></a>' +
+                    '</div>' +
+                '</div>'
+            );
+
+            $el.appendTo('body');
+
+            bindClickToLinkInitializerWidget(null, $el);
+
+            $el.find('.span-outside-inner-anchor').click();
+
+            expect(bindClickToLinkInitializerWidget.redirectTo).not.toHaveBeenCalled();
+        });
+
+        it('Should not navigate away from page if it is missing data-link-type attribute', function () {
+            spyOn(bindClickToLinkInitializerWidget, 'redirectTo');
+            $el = $(
+                '<div data-role="content-type">' +
+                    '<div href="https://adobe.com">' +
+                        '<span class="span-outside-inner-anchor">Hello world</span>' +
+                        '<a class="inner-anchor" href="https://something.com"><span>Something</span></a>' +
+                    '</div>' +
+                '</div>'
+            );
+
+            $el.appendTo('body');
+
+            bindClickToLinkInitializerWidget(null, $el);
+
+            $el.find('.span-outside-inner-anchor').click();
+
+            expect(bindClickToLinkInitializerWidget.redirectTo).not.toHaveBeenCalled();
+        });
+
+        it('Should not navigate to an simulated anchor\'s href if clicked inside of nested anchor', function () {
+            spyOn(bindClickToLinkInitializerWidget, 'redirectTo');
+
+            $el = $(
+                '<div data-role="content-type">' +
+                    '<div data-link-type="link" href="https://adobe.com">' +
+                        '<span class="span-outside-inner-anchor">Hello world</span>' +
+                        '<a class="inner-anchor" href="https://something.com"><span>Something</span></a>' +
+                    '</div>' +
+                '</div>'
+            );
+
+            $el.appendTo('body');
+
+            bindClickToLinkInitializerWidget(null, $el);
+
+            $el.find('.inner-anchor').click();
+
+            expect(bindClickToLinkInitializerWidget.redirectTo).not.toHaveBeenCalled();
+        });
+
+        it('Should navigate to an simulated anchor\'s href if clicked outside of nested anchor', function () {
+            spyOn(bindClickToLinkInitializerWidget, 'redirectTo');
+
+            $el = $(
+                '<div data-role="content-type">' +
+                    '<div data-link-type="link" href="https://adobe.com">' +
+                        '<span class="span-outside-inner-anchor">Hello world</span>' +
+                        '<a class="inner-anchor" href="https://something.com"><span>Something</span></a>' +
+                    '</div>' +
+                '</div>'
+            );
+
+            $el.appendTo('body');
+
+            bindClickToLinkInitializerWidget(null, $el);
+
+            $el.find('.span-outside-inner-anchor').click();
+
+            expect(bindClickToLinkInitializerWidget.redirectTo).toHaveBeenCalledWith('https://adobe.com');
+        });
+    });
+});
