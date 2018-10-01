b220b0c08e375dc10f84d03608347c9a6c2a292e
Anthoula Wojczak <awojczak@adobe.com>
2018-10-01 17:17:16 -0500

    MC-4116: Update slider to use new background images logic
    - rename background-image file for use with all content types

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/converter/style/background-image.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/converter/style/background-image.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-define(["Magento_PageBuilder/js/utils/image"], function (_image) {
+define([], function () {
   /**
    * Copyright © Magento, Inc. All rights reserved.
    * See COPYING.txt for license details.
@@ -22,11 +22,7 @@ define(["Magento_PageBuilder/js/utils/image"], function (_image) {
      * @returns {string | object}
      */
     _proto.fromDom = function fromDom(value) {
-      if (!value) {
-        return "";
-      }
-
-      return (0, _image.decodeUrl)(value);
+      return null;
     };
     /**
      * Convert value to knockout format
@@ -38,14 +34,7 @@ define(["Magento_PageBuilder/js/utils/image"], function (_image) {
 
 
     _proto.toDom = function toDom(name, data) {
-      var value = data[name];
-
-      if (value[0] === undefined || value[0].url === undefined) {
-        return "";
-      }
-
-      var imageUrl = value[0].url;
-      return (0, _image.imageToBackgroundImageDataUrl)(imageUrl);
+      return null;
     };
 
     return BackgroundImage;
