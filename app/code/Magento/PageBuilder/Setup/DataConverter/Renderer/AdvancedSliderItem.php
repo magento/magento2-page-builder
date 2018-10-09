a528a534272c1c90aa21196239b3d09fb3fb5ea6
Anthoula Wojczak <awojczak@adobe.com>
2018-10-09 14:05:01 -0500

    MC-4116: Update slider to use new background images logic
    - update data migration script and tests

--- app/code/Magento/PageBuilder/Setup/DataConverter/Renderer/AdvancedSliderItem.php
+++ app/code/Magento/PageBuilder/Setup/DataConverter/Renderer/AdvancedSliderItem.php
@@ -59,11 +59,27 @@ class AdvancedSliderItem implements RendererInterface
         ];
 
         $formData = $itemData['formData'] ?? [];
-        $formData['background_image'] = '';
+        $backgroundImages = '';
         if (isset($eavData['background_image'])) {
-            $formData['background_image'] = $eavData['background_image'];
+            $backgroundImages = $eavData['background_image'];
         } elseif (isset($eavData['image'])) {
-            $formData['background_image'] = $eavData['image'];
+            $backgroundImages = $eavData['image'];
+        }
+
+        if ($backgroundImages) {
+            $backgroundImagesAttr = '{\&quot;'
+                . 'desktop_image\&quot;:\&quot;'
+                . '{{media url=wysiwyg'
+                . $backgroundImages
+                . '}}\&quot;,\&quot;'
+                . 'mobile_image\&quot;:\&quot;'
+                . '{}\&quot;}';
+        } else {
+            $backgroundImagesAttr = '{\&quot;'
+                . 'desktop_image\&quot;:\&quot;'
+                . '{}\&quot;,\&quot;'
+                . 'mobile_image\&quot;:\&quot;'
+                . '{}\&quot;}';
         }
 
         $margin = $this->styleExtractor->extractStyle($formData, ['margin']);
@@ -72,7 +88,8 @@ class AdvancedSliderItem implements RendererInterface
         }
 
         $wrapperDivElementAttributes = [
-            'data-element' => 'desktop_image',
+            'data-element' => 'wrapper',
+            'data-background-images' => $backgroundImagesAttr,
             'class' => 'pagebuilder-slide-wrapper'
         ];
         $style = $this->styleExtractor->extractStyle($formData);
@@ -116,21 +133,6 @@ class AdvancedSliderItem implements RendererInterface
             . '><div class="pagebuilder-poster-content">'
             . '<div data-element="content">'
             . '<h3>'
-            . ($eavData['title'] ?? $eavData['title_tag'] ?? '')
-            . '</h3>'
-            . '<div>' . ($eavData['textarea'] ?? '') . '</div></div>'
-            . $buttonElementHtml
-            . '</div></div></div>';
-
-        // non-mobile wrapper div
-        $wrapperDivElementAttributes['data-element'] = 'mobile_image';
-        $wrapperDivElementAttributes['class'] = 'pagebuilder-slide-wrapper';
-        $rootElementHtml .= '<div'
-            . $this->printAttributes($wrapperDivElementAttributes)
-            . '><div'
-            . $this->printAttributes($overlayDivElementAttributes)
-            . '><div class="pagebuilder-poster-content">'
-            . '<div data-element="content"><h3>'
             . ($eavData['title'] ?? $eavData['title_tag'] ?? '')
             . '</h3>'
             . '<div>' . ($eavData['textarea'] ?? '') . '</div></div>'
