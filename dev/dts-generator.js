2c1d69c44e149a6170c7dfdcbfcbb5b23c86e5f9
Dave Macaulay <dmacaulay@magento.com>
2019-01-22 14:12:54 +0100

    MC-10833: Resolve TypeScript errors
    - Improve DTS generation

--- dev/dts-generator.js
+++ dev/dts-generator.js
@@ -3,7 +3,14 @@
  * See COPYING.txt for license details.
  */
 
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
+const prettier = require('prettier');
+const typesFile = 'page-builder-types/index.d.ts';
+const copyrightComment = `/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */`;
 
 /**
  * Resolve a file system path to the Magento module import path
@@ -14,32 +21,50 @@ var path = require('path');
 function resolveModuleIdToMagentoPath(currentModuleId) {
     return currentModuleId.replace(
         'app/code/Magento/PageBuilder/view/adminhtml/web/ts/',
-        'Magento_PageBuilder'
+        'Magento_PageBuilder/'
     );
 }
 
-const typePrefix = `/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-/* tslint:disable */`;
-
+// Use dts-generator to create a single types definition file
 require('dts-generator').default({
     project: './',
-    out: 'page-builder-types/index.d.ts',
+    out: typesFile,
     resolveModuleId: (params) => {
         return resolveModuleIdToMagentoPath(params.currentModuleId);
     },
     resolveModuleImport: (params) => {
-        return resolveModuleIdToMagentoPath(
-            path.resolve(
-                path.dirname(params.currentModuleId),
-                params.importedModuleId,
-            ).replace(
-                process.cwd() + '/',
-                ''
-            )
-        );
+        // Convert relative imports into their Magento counterparts
+        if (params.importedModuleId.startsWith('../') || params.importedModuleId.startsWith('./')) {
+            return resolveModuleIdToMagentoPath(
+                path.resolve(
+                    path.dirname(params.currentModuleId),
+                    params.importedModuleId,
+                ).replace(
+                    process.cwd() + '/',
+                    ''
+                )
+            );
+        }
+        return params.importedModuleId;
     }
+}).then(() => {
+    const { exec } = require('child_process');
+    // Lint the generated file
+    const lint = exec(`./node_modules/tslint/bin/tslint --fix ${typesFile}`);
+    lint.on("exit", () => {
+        // Replace all tab characters with 4 spaces
+        fs.readFile(typesFile, 'utf-8', (error, contents) => {
+            if (error) {
+                throw Error(`Unable to read types file ${typesFile}.`);
+            }
+            let modifiedContents = contents
+                .replace(/.*\/\*\*\n.*Copyright © Magento.*\n.*\n.*\*\//gm, '') // Strip all Magento copyright
+                .replace(/.*\/\*\*\n.*@api.*\n.*\*\//gm, ''); // Strip all @api comments
+            modifiedContents = `${copyrightComment}\n/* tslint:disable */\n${modifiedContents}`;
+            fs.writeFile(typesFile, prettier.format(modifiedContents, {parser: "typescript"}), null, () => {
+                console.log("Type definition generation completed.");
+                process.exit();
+            });
+        });
+    });
 });
