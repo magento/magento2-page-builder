ebcc76938cb647a7ac6e5eb51ed6631d9ce21b33
Dan Mooney <dmooney@adobe.com>
2019-03-08 10:48:26 -0600

    MC-15311: [Sec] PageBuilder XSS Injection Possible Through Block on HTML Code Content Type For CSS Classes Attribute & in TinyMCE
    Add custom variable filter test case

---
+++ dev/tests/integration/testsuite/Magento/PageBuilder/_files/custom_variable_xss.php
@@ -0,0 +1,18 @@
+<?php
+
+$this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+/** @var \Magento\Variable\Model\ResourceModel\Variable $variableResource */
+$variableResource = $this->objectManager->get(\Magento\Variable\Model\ResourceModel\Variable::class);
+
+/** @var \Magento\Variable\Model\Variable $variable */
+$variable = $this->objectManager->get(\Magento\Variable\Model\Variable::class);
+
+$variable->setData([
+    'code' => 'xssVariable',
+    'name' => 'xssVariable',
+    'html_value' => '<img src=x onerror="alert(0)">',
+    'plain_value' => '<img src=x onerror="alert(0)">',
+]);
+
+$variableResource->save($variable);
