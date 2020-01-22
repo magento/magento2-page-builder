e7797ad01a4a73f600589047013e4106e67ce211
Dave Macaulay <macaulay@adobe.com>
2020-01-22 09:52:47 -0600

    PB-366: Templates Lower Level Test Coverage
    - Block Templates page if Page Builder is disabled

--- app/code/Magento/PageBuilder/Controller/Adminhtml/Template/Index.php
+++ app/code/Magento/PageBuilder/Controller/Adminhtml/Template/Index.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 
 namespace Magento\PageBuilder\Controller\Adminhtml\Template;
 
-use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Backend\App\Action\Context;
-use Magento\Framework\View\Result\PageFactory;
 use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\PageBuilder\Model\Config;
+use Magento\Framework\Controller\Result\ForwardFactory;
 
 /**
  * Display template grid
@@ -26,15 +28,31 @@ class Index extends Action implements HttpGetActionInterface
     private $resultPageFactory;
 
     /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var ForwardFactory
+     */
+    private $forwardFactory;
+
+    /**
      * @param Context $context
      * @param PageFactory $resultPageFactory
+     * @param Config $config
+     * @param ForwardFactory $forwardFactory
      */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        Config $config,
+        ForwardFactory $forwardFactory
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->config = $config;
+        $this->forwardFactory = $forwardFactory;
     }
 
     /**
@@ -44,6 +62,10 @@ class Index extends Action implements HttpGetActionInterface
      */
     public function execute()
     {
+        if (!$this->config->isEnabled()) {
+            return $this->forwardFactory->create()->forward('noroute');
+        }
+
         /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
         $resultPage = $this->resultPageFactory->create();
         $resultPage->setActiveMenu('Magento_PageBuilder::templates');
