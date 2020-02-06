f89ebab7daba15a52a52e45bb4e71953078189eb
Ievgen Kolesov <ikolesov@adobe.com>
2020-02-06 14:07:57 -0600

    PB-95: Video background for Row

--- dev/tests/js/jasmine/tests/app/code/Magento/PageBuilder/view/frontend/web/js/content-type/row/appearance/default/widget.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/PageBuilder/view/frontend/web/js/content-type/row/appearance/default/widget.test.js
@@ -8,24 +8,26 @@ define([
     'use strict';
 
     describe('Magento_PageBuilder/js/content-type/row/appearance/default/widget', function () {
-        it('Should call not call jarallax if enableParallax !== 1', function () {
+        it('Should not call jarallax if enableParallax !== 1 and image background', function () {
             var el = document.createElement('div');
 
             spyOn(window, 'jarallax');
 
-            el.setAttribute('data-enable-parallax', 0);
+            el.setAttribute('data-enable-parallax', '0');
+            el.setAttribute('data-background-type', 'image');
 
             rowWidgetInitializer(undefined, el);
 
             expect(window.jarallax).not.toHaveBeenCalled();
         });
 
-        it('Should call call jarallax if enableParallax === 1', function () {
+        it('Should call call jarallax if enableParallax === 1 and image background', function () {
             var el = document.createElement('div');
 
             spyOn(window, 'jarallax');
 
-            el.setAttribute('data-enable-parallax', 1);
+            el.setAttribute('data-enable-parallax', '1');
+            el.setAttribute('data-background-type', 'image');
 
             rowWidgetInitializer(undefined, el);
 
@@ -37,12 +39,13 @@ define([
 
             spyOn(window, 'jarallax');
 
-            el.setAttribute('data-enable-parallax', 1);
+            el.setAttribute('data-enable-parallax', '1');
+            el.setAttribute('data-background-type', 'image');
 
             el.style.backgroundPosition = '0px 50%';
             el.style.backgroundRepeat = 'repeat';
             el.style.backgroundSize = '100%';
-            el.setAttribute('data-parallax-speed', 1);
+            el.setAttribute('data-parallax-speed', '1');
 
             rowWidgetInitializer(undefined, el);
 
@@ -51,6 +54,62 @@ define([
                 imgRepeat: 'repeat',
                 imgSize: '100%',
                 speed: 1
+            });
+        });
+
+        it('Should call jarallax if video background and enableParallax === 1', function () {
+            var el = document.createElement('div');
+
+            spyOn(window, 'jarallax');
+
+            el.setAttribute('data-enable-parallax', '1');
+            el.setAttribute('data-background-type', 'video');
+
+            rowWidgetInitializer(undefined, el);
+
+            expect(window.jarallax).toHaveBeenCalled();
+        });
+
+        it('Should call jarallax if video background and parallax not enabled with speed = 1', function () {
+            var el = document.createElement('div');
+
+            spyOn(window, 'jarallax');
+
+            el.setAttribute('data-enable-parallax', '0');
+            el.setAttribute('data-background-type', 'video');
+
+            rowWidgetInitializer(undefined, el);
+
+            expect(window.jarallax).toHaveBeenCalledWith(el, {
+                speed: 1,
+                imgSrc: undefined,
+                videoLoop: undefined,
+                videoPlayOnlyVisible: undefined,
+                disableVideo: false
+            });
+        });
+
+        it('Should call jarallax on element based on its data if video background', function () {
+            var el = document.createElement('div');
+
+            spyOn(window, 'jarallax');
+
+            el.setAttribute('data-enable-parallax', '1');
+            el.setAttribute('data-background-type', 'video');
+            el.setAttribute('data-enable-parallax', '1');
+            el.setAttribute('data-video-fallback-src', '/image/test.jpg');
+            el.setAttribute('data-video-loop', 'true');
+            el.setAttribute('data-video-play-only-visible', 'false');
+            el.setAttribute('data-parallax-speed', '0.3');
+
+            rowWidgetInitializer(undefined, el);
+
+            expect(window.jarallax).toHaveBeenCalledWith(el, {
+                speed: 0.3,
+                imgSrc: '/image/test.jpg',
+                videoLoop: true,
+                videoPlayOnlyVisible: false,
+                disableVideo: false
             });
         });
     });
