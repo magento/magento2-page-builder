4a331f8a2ad9672dec817fc666db6c99a1f3c74e
Marc Ginesta <mginesta@adobe.com>
2020-07-31 12:56:03 +0200

    MC-35103: Tracking Page Builder actions - Refactor: WIP Extract getAction

--- app/code/Magento/PageBuilderAdminAnalytics/view/adminhtml/web/js/page-builder/events-mixin.js
+++ app/code/Magento/PageBuilderAdminAnalytics/view/adminhtml/web/js/page-builder/events-mixin.js
@@ -10,6 +10,7 @@ define(['underscore'], function (_underscore) {
         var originalTarget = target.trigger,
             isAdminAnalyticsEnabled,
             action = '',
+            getAction,
             event;
 
         /**
@@ -25,8 +26,6 @@ define(['underscore'], function (_underscore) {
                 !_underscore.isUndefined(window.digitalData) &&
                 !_underscore.isUndefined(window._satellite);
 
-            console.log(name, args);
-
             if (name.indexOf('readyAfter') !== -1 && isAdminAnalyticsEnabled) {
                 window.digitalData.page.url = window.location.href;
                 window.digitalData.page.attributes = {
@@ -35,26 +34,13 @@ define(['underscore'], function (_underscore) {
                 window._satellite.track('page');
             }
 
-            if (name.indexOf('duplicateAfter') !== -1) action='duplicate';
-            if (name.indexOf('removeAfter') !== -1) action='remove';
-            if (name.indexOf('createAfter') !== -1) action='create';
-
-            if (name.indexOf('updateAfter') !== -1 &&
-                !_underscore.isUndefined(args) &&
-                !_underscore.isUndefined(args.dataStore) &&
-                !_underscore.isUndefined(args.dataStore.previousState) &&
-                !_underscore.isUndefined(args.dataStore.state)
-            ) {
-                console.log('hiding/showing');
-                if (args.dataStore.previousState.display === true &&
-                    args.dataStore.state.display === false) action = 'hide';
-                if (args.dataStore.previousState.display === false &&
-                    args.dataStore.state.display === true) action = 'show';
-            }
+            console.log("antes", name);
+            action = getAction(name, args);
 
             if (!_underscore.isUndefined(args) && !_underscore.isUndefined(args.contentType) &&
                 !_underscore.isUndefined(args.contentType.config && action !== '')
             ) {
+                console.log('justo antes de event');
                 event = {
                     element: args.contentType.config.label,
                     type: args.contentType.config.name,
@@ -66,13 +52,52 @@ define(['underscore'], function (_underscore) {
                     feature: 'page-builder-tracker'
                 };
 
-                console.log('EVENT:', event);
+                console.log('dentro', event);
 
                 if (isAdminAnalyticsEnabled && !_underscore.isUndefined(window.digitalData.event)) {
                     window.digitalData.event.push(event);
                     window._satellite.track('event');
                 }
             }
+        };
+
+        getAction = function (name, args) {
+            var triggeredAction = '',
+                arrayName,
+                arrayNameObject,
+                state,
+                previousState;
+
+            if (name.indexOf('duplicateAfter') !== -1) triggeredAction = 'duplicate';
+
+            if (name.indexOf('removeAfter') !== -1) triggeredAction = 'remove';
+
+            if (name.indexOf('createAfter') !== -1) triggeredAction = 'create';
+
+            if (name.indexOf('renderAfter') !== -1) triggeredAction = 'edit';
+
+            if (name.indexOf('updateAfter') !== -1) {
+                arrayName = name.split(':');
+
+                if (arrayName.length === 3) {
+                    arrayNameObject = arrayName[1];
+
+                    if (!_underscore.isUndefined(args[arrayNameObject]) &&
+                        !_underscore.isUndefined(args[arrayNameObject]).dataStore
+                    ) {
+                        previousState = !_underscore.isUndefined(args[arrayNameObject].dataStore.previousState) ?
+                            args[arrayNameObject].dataStore.previousState.display : '';
+                        state = !_underscore.isUndefined(args[arrayNameObject].dataStore.state) ?
+                            args[arrayNameObject].dataStore.state.display : '';
+
+                        if (previousState === true && state === false) triggeredAction = 'hide';
+                        else if (previousState === false && state === true) triggeredAction = 'show';
+                        else triggeredAction = '';
+                    }
+                }
+            }
+
+            return triggeredAction;
         };
 
         return target;
