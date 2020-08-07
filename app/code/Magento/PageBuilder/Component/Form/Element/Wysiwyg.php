2b2658dc06803cee4a134ff3b82b2cf797402dc8
Matt Walters <bluemwhitew@hotmail.co.uk>
2020-08-07 20:50:24 +0100

    magento/magento2-page-builder#544: Configurable Page Builder Full Screen Mode - Fixing Static Test(s)

--- app/code/Magento/PageBuilder/Component/Form/Element/Wysiwyg.php
+++ app/code/Magento/PageBuilder/Component/Form/Element/Wysiwyg.php
@@ -67,7 +67,7 @@ class Wysiwyg extends \Magento\Ui\Component\Form\Element\Wysiwyg
                     $config['wysiwyg'] = (bool)$attribute->getIsWysiwygEnabled();
                 }
             } catch (NoSuchEntityException $e) {
-                throw new NoSuchEntityException();
+                $config['wysiwyg'] = false;
             }
         }
 
