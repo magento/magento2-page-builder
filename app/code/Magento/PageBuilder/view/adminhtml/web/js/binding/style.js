0a44f7d4fa8084e871a8bb31d74a1fb9f45a5d19
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2020-08-28 11:47:44 -0500

    Implement inline styles conversion - add stage to bindingContext - extend style binding - revert pbStyle to ko-style

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/binding/style.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/binding/style.js
@@ -13,39 +13,51 @@ define(["knockout", "mageUtils", "underscore", "Magento_PageBuilder/js/content-t
    * Copyright © Magento, Inc. All rights reserved.
    * See COPYING.txt for license details.
    */
-  _knockout.default.bindingHandlers.pbStyles = {
-    init: function init(element) {
-      element.setAttribute("data-style-id", _mageUtils.default.uniqueid());
-    },
-    update: function update(element, valueAccessor) {
-      var value = _knockout.default.utils.unwrapObservable(valueAccessor() || {});
+  var originalStyle = _knockout.default.bindingHandlers.style;
 
-      var styles = {};
-      var className = "style-" + element.getAttribute("data-style-id");
-      var existedStyleBlock = document.querySelector("[data-selector=\"" + className + "\"]");
+  function isPageBuilderContext(context) {
+    return !!(context.stage && context.stage.pageBuilder);
+  }
 
-      _knockout.default.utils.objectForEach(value, function (styleName, styleValue) {
-        styleValue = _knockout.default.utils.unwrapObservable(styleValue);
-
-        if (styleValue === null || styleValue === undefined || styleValue === false) {
-          styleValue = "";
-        }
-
-        if (styleValue) {
-          styles[styleName] = styleValue;
-        }
-      });
-
-      if (existedStyleBlock) {
-        existedStyleBlock.remove();
+  _knockout.default.bindingHandlers.style = {
+    init: function init(element, valueAccessor, allBindings, viewModel, bindingContext) {
+      if (isPageBuilderContext(bindingContext)) {
+        element.setAttribute("data-style-id", _mageUtils.default.uniqueid());
       }
+    },
+    update: function update(element, valueAccessor, allBindings, viewModel, bindingContext) {
+      if (isPageBuilderContext(bindingContext)) {
+        var value = _knockout.default.utils.unwrapObservable(valueAccessor() || {});
 
-      if (!_underscore.default.isEmpty(styles)) {
-        var styleElement = document.createElement("style");
-        styleElement.setAttribute("data-selector", className);
-        styleElement.innerHTML = (0, _styleRegistry.generateCssBlock)(className, styles);
-        element.classList.add(className);
-        element.append(styleElement);
+        var styles = {};
+        var className = "style-" + element.getAttribute("data-style-id");
+        var existedStyleBlock = document.querySelector("[data-selector=\"" + className + "\"]");
+
+        _knockout.default.utils.objectForEach(value, function (styleName, styleValue) {
+          styleValue = _knockout.default.utils.unwrapObservable(styleValue);
+
+          if (styleValue === null || styleValue === undefined || styleValue === false) {
+            styleValue = "";
+          }
+
+          if (styleValue) {
+            styles[styleName] = styleValue;
+          }
+        });
+
+        if (existedStyleBlock) {
+          existedStyleBlock.remove();
+        }
+
+        if (!_underscore.default.isEmpty(styles)) {
+          var styleElement = document.createElement("style");
+          styleElement.setAttribute("data-selector", className);
+          styleElement.innerHTML = (0, _styleRegistry.generateCssBlock)(className, styles);
+          element.classList.add(className);
+          element.append(styleElement);
+        }
+      } else {
+        originalStyle.update(element, valueAccessor, allBindings, viewModel, bindingContext);
       }
     }
   };
--- app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/binding/style.ts
+++ app/code/Magento/PageBuilder/view/adminhtml/web/ts/js/binding/style.ts
@@ -8,38 +8,49 @@ import utils from "mageUtils";
 import _ from "underscore";
 import {generateCssBlock} from "../content-type/style-registry";
 
-ko.bindingHandlers.pbStyles = {
-    init: (element) => {
-        element.setAttribute("data-style-id", utils.uniqueid());
+const originalStyle: KnockoutBindingHandler = ko.bindingHandlers.style;
+
+function isPageBuilderContext(context: {[key: string]: any}): boolean {
+    return !!(context.stage && context.stage.pageBuilder);
+}
+
+ko.bindingHandlers.style = {
+    init: (element, valueAccessor, allBindings, viewModel, bindingContext) => {
+        if (isPageBuilderContext(bindingContext)) {
+            element.setAttribute("data-style-id", utils.uniqueid());
+        }
     },
-    update: (element: HTMLElement, valueAccessor) => {
-        const value = ko.utils.unwrapObservable(valueAccessor() || {});
-        const styles: { [key: string]: any; } = {};
-        const className = "style-" + element.getAttribute("data-style-id");
-        const existedStyleBlock = document.querySelector(`[data-selector="${className}"]`);
+    update: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
+        if (isPageBuilderContext(bindingContext)) {
+            const value = ko.utils.unwrapObservable(valueAccessor() || {});
+            const styles: { [key: string]: any; } = {};
+            const className = "style-" + element.getAttribute("data-style-id");
+            const existedStyleBlock = document.querySelector(`[data-selector="${className}"]`);
 
-        ko.utils.objectForEach(value, (styleName, styleValue) => {
-            styleValue = ko.utils.unwrapObservable(styleValue);
+            ko.utils.objectForEach(value, (styleName, styleValue) => {
+                styleValue = ko.utils.unwrapObservable(styleValue);
 
-            if (styleValue === null || styleValue === undefined || styleValue === false) {
-                styleValue = "";
+                if (styleValue === null || styleValue === undefined || styleValue === false) {
+                    styleValue = "";
+                }
+                if (styleValue) {
+                    styles[styleName] = styleValue;
+                }
+            });
+
+            if (existedStyleBlock) {
+                existedStyleBlock.remove();
             }
-            if (styleValue) {
-                styles[styleName] = styleValue;
+            if (!_.isEmpty(styles)) {
+                const styleElement: HTMLStyleElement = document.createElement("style");
+
+                styleElement.setAttribute("data-selector", className);
+                styleElement.innerHTML = generateCssBlock(className, styles);
+                element.classList.add(className);
+                element.append(styleElement);
             }
-        });
-
-        if (existedStyleBlock) {
-            existedStyleBlock.remove();
+        } else {
+            originalStyle.update(element, valueAccessor, allBindings, viewModel, bindingContext);
         }
-        if (!_.isEmpty(styles)) {
-            const styleElement: HTMLStyleElement = document.createElement("style");
-
-            styleElement.setAttribute("data-selector", className);
-            styleElement.innerHTML = generateCssBlock(className, styles);
-            element.classList.add(className);
-            element.append(styleElement);
-        }
-
     },
 };
