9274f0fa0433053674bd437dfaad55f2e187c47c
Buba Suma <soumah@adobe.com>
2021-04-09 09:09:04 -0500

    MC-41478: [Page builder - Products] Content preview breaks if subcategories have different position for same SKU
    - Fix page builder preview when products is filtered by category and products have different positions in the selected category subcategories

--- app/code/Magento/PageBuilder/Ui/DataProvider/Product/ProductCollection.php
+++ app/code/Magento/PageBuilder/Ui/DataProvider/Product/ProductCollection.php
@@ -38,4 +38,34 @@ class ProductCollection extends \Magento\Catalog\Model\ResourceModel\Product\Col
         $this->_productLimitationFilters->setUsePriceIndex($this->getStoreId() !== Store::DEFAULT_STORE_ID);
         return $this->_productLimitationPrice(false);
     }
+
+    /**
+     * @inheritdoc
+     */
+    protected function _applyZeroStoreProductLimitations()
+    {
+        $conditions = [
+            'cat_pro.product_id=e.entity_id',
+            $this->getConnection()->quoteInto(
+                'cat_pro.category_id = ?',
+                $this->_productLimitationFilters['category_id'],
+                \Zend_Db::INT_TYPE
+            ),
+        ];
+        $joinCond = join(' AND ', $conditions);
+        $fromPart = $this->getSelect()->getPart(\Magento\Framework\DB\Select::FROM);
+        if (isset($fromPart['cat_pro'])) {
+            $fromPart['cat_pro']['joinCondition'] = $joinCond;
+            $this->getSelect()->setPart(\Magento\Framework\DB\Select::FROM, $fromPart);
+        } else {
+            $this->getSelect()->joinLeft(
+                ['cat_pro' => $this->getTable('catalog_category_product')],
+                $joinCond,
+                ['cat_index_position' => $this->getConnection()->getIfNullSql('position', '~0')]
+            );
+        }
+        $this->_joinFields['position'] = ['table' => 'cat_pro', 'field' => 'position'];
+
+        return $this;
+    }
 }
