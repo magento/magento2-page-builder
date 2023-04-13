976ac7f2467c5c7a39c440bb9b5a176ad1a69458
Ajay Prajapati <130637952+aj-prajapati@users.noreply.github.com>
2023-04-13 18:42:58 +0530

    Width & Height attribute - Pagebuilder image #767
    Width and Height attribute added for Pagebuilder image element 
    
    https://github.com/magento/magento2-page-builder/issues/767

---
+++ app/code/Magento/PageBuilderImageAttribute/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Changes by: Ajay Prajapati
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_PageBuilderImageAttribute', __DIR__);
