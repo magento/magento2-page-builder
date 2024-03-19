04b8d9db1afde9cf1aacfb559cad2e307f1af412
glo24157 <glo24157@adobe.com>
2024-03-19 13:32:32 +0530

    Fix static tests failures

--- app/code/Magento/PageBuilderImageAttribute/registration.php
+++ app/code/Magento/PageBuilderImageAttribute/registration.php
@@ -4,6 +4,8 @@
  * Changes by: Ajay Prajapati
  */
 
+declare(strict_types=1);
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_PageBuilderImageAttribute', __DIR__);
