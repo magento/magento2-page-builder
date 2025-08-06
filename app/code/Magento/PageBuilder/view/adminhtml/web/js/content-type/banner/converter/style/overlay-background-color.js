089a8f7dee8d578fab6747af523ca4f7616c535a
Nayan Chavda <nayan.c@conversio.in>
2025-08-06 10:41:47 +0530

    Updated file with copyright information

--- app/code/Magento/PageBuilder/view/adminhtml/web/js/content-type/banner/converter/style/overlay-background-color.js
+++ app/code/Magento/PageBuilder/view/adminhtml/web/js/content-type/banner/converter/style/overlay-background-color.js
@@ -2,8 +2,8 @@
 /* jscs:disable */
 define(["Magento_PageBuilder/js/utils/object"], function (_object) {
   /**
-   * Copyright © Magento, Inc. All rights reserved.
-   * See COPYING.txt for license details.
+   * Copyright 2025 Adobe
+   * All Rights Reserved.
    */
   var OverlayBackgroundColor = /*#__PURE__*/function () {
     "use strict";
